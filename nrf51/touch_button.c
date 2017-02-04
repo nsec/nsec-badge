e27e528b112e5d87ef7aa027dbfdb23150702263
Marc-Etienne M.Léveillé <marc.etienne.ml@gmail.com>
2017-02-04 00:16:53 -0500

    Disable ratelimiting of touch buttons because app_timer_cnt_get always returns zero.

--- nrf51/touch_button.c
+++ nrf51/touch_button.c
@@ -39,9 +39,10 @@ void touch_on_event(enum touch_event event, enum touch_button button) {
     event_received_time = app_timer_cnt_get();
     app_timer_cnt_diff_compute(event_received_time, last_event_received_time, &event_received_diff);
     last_event_received_time = event_received_time;
-    if(event_received_diff < 3000) {
-        return;
-    }
+    // Disable ratelimiting for now because app_timer_cnt_get() always returns 0
+    // if(event_received_diff < 3000) {
+    //    return;
+    // }
 
     if(event == TOUCH_EVENT_DOWN) {
         button_t button_pressed;
