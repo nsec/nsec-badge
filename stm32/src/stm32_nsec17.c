ca982cd19ad9753ef61d74f227893ee6da92cb8a
Marc-Etienne M.Léveillé <marc-etienne@nsec.io>
2017-05-14 12:58:47 -0400

    Disable unused tim3 for now

--- stm32/src/stm32_nsec17.c
+++ stm32/src/stm32_nsec17.c
@@ -14,39 +14,7 @@ static void clock_setup(void) {
 
     rcc_periph_clock_enable(RCC_GPIOA);
     rcc_periph_clock_enable(RCC_GPIOB);
-    rcc_periph_clock_enable(RCC_TIM3);
 }
-
-
-
-static volatile int t3ovf;
-
-// TIM3 - ping every ms
-void tim3_isr(void) {
-    TIM_SR(TIM3) &= ~TIM_SR_UIF;
-    if (t3ovf++ > 100) {
-        // TODO - handle scheduler here
-
-        // Reset the timer overflow flag.
-        t3ovf = 0;
-    }
-}
-
-static void setup_tim3(void) {
-    rcc_periph_reset_pulse(RST_TIM3);
-    // Set the timer to 1ms interrupt
-    // 48 MHz / 10 kHz -> prescaler = 4800 Hz
-    timer_set_prescaler(TIM3, 4800);
-    // 10kHz / 10 ticks = 1 kHz overflow = 1ms overflow interrupts
-    timer_set_period(TIM3, 10);
-
-    nvic_enable_irq(NVIC_TIM3_IRQ);
-    timer_enable_update_event(TIM3);
-    timer_enable_irq(TIM3, TIM_DIER_UIE);
-    timer_enable_counter(TIM3);
-}
-
-
 
 void say_hello(void);
 
@@ -56,7 +24,6 @@ void say_hello(void) {
 
 int main(void) {
     clock_setup();
-    setup_tim3();
 
     touch_init();
     usart_init();
