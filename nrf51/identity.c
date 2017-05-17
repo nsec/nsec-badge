cd77f60eef5bd3ce4ec72143c103df1f282727d9
Marc-Etienne M.Léveillé <marc-etienne@nsec.io>
2017-05-17 00:05:48 -0400

    Remove the XP stuff, we won't need it

--- nrf51/identity.c
+++ nrf51/identity.c
@@ -19,10 +19,7 @@
     ((NSEC_IDENTITY_AVATAR_WIDTH * NSEC_IDENTITY_AVATAR_HEIGHT + 1) / 8)
 
 typedef struct {
-    char name[32];
-    uint8_t xp_reverse;
-    uint8_t xp_crypto;
-    uint8_t xp_web;
+    char name[16];
     uint8_t unlocked;
     uint8_t avatar[AVATAR_SIZE];
 } nsec_identity_t;
@@ -31,14 +28,12 @@ static nsec_identity_t identity;
 static nsec_ble_service_handle identity_ble_handle;
 
 const uint8_t identity_ble_uuid[16] = {
+    // 6AA99996-93B3-95BA-BC40-42684CDB36FB
     0xFB, 0x36, 0xDB, 0x4C, 0x68, 0x42, 0x40, 0xBC,
     0xBA, 0x95, 0xB3, 0x93, 0x99, 0x96, 0xA9, 0x6A
 };
 enum {
     IDENTITY_CHAR_UUID_NAME = 0x6e6d,
-    IDENTITY_CHAR_UUID_XP_REVERSE = 0x7870,
-    IDENTITY_CHAR_UUID_XP_CRYPTO,
-    IDENTITY_CHAR_UUID_XP_WEB,
     IDENTITY_CHAR_UUID_AVATAR = 0x6176,
     IDENTITY_CHAR_UUID_UNLOCK_KEY = 0x6b79,
     IDENTITY_CHAR_UUID_IS_UNLOCKED,
@@ -55,11 +50,8 @@ static void nsec_identity_ble_callback(nsec_ble_service_handle service, uint16_t
 
 void nsec_identitiy_init(void) {
     memset(identity.name, 0, sizeof(identity.name));
-    strncpy(identity.name, "<default name>", sizeof(identity.name));
+    snprintf(identity.name, sizeof(identity.name), "Comrade #%05d", (NRF_FICR->DEVICEID[0] & 0xFFFF));
     memcpy(identity.avatar, default_avatar_bitmap, sizeof(identity.avatar));
-    identity.xp_reverse = 0;
-    identity.xp_crypto = 0;
-    identity.xp_web = 0;
     identity.unlocked = 0;
 
     nsec_ble_characteristic_t c[] = {
@@ -67,21 +59,6 @@ void nsec_identitiy_init(void) {
             .char_uuid = IDENTITY_CHAR_UUID_NAME,
             .permissions = NSEC_BLE_CHARACT_PERM_RW,
             .max_size = sizeof(identity.name),
-            .on_write = nsec_identity_ble_callback,
-        }, {
-            .char_uuid = IDENTITY_CHAR_UUID_XP_REVERSE,
-            .permissions = NSEC_BLE_CHARACT_PERM_RW,
-            .max_size = 1,
-            .on_write = nsec_identity_ble_callback,
-        }, {
-            .char_uuid = IDENTITY_CHAR_UUID_XP_CRYPTO,
-            .permissions = NSEC_BLE_CHARACT_PERM_RW,
-            .max_size = 1,
-            .on_write = nsec_identity_ble_callback,
-        }, {
-            .char_uuid = IDENTITY_CHAR_UUID_XP_WEB,
-            .permissions = NSEC_BLE_CHARACT_PERM_RW,
-            .max_size = 1,
             .on_write = nsec_identity_ble_callback,
         }, {
             .char_uuid = IDENTITY_CHAR_UUID_AVATAR,
@@ -108,9 +85,6 @@ void nsec_identitiy_init(void) {
     nsec_ble_register_vendor_service(&srv, &identity_ble_handle);
 
     UPDATE_BLE_CHARACTERISTIC(IDENTITY_CHAR_UUID_NAME, identity.name);
-    UPDATE_BLE_CHARACTERISTIC(IDENTITY_CHAR_UUID_XP_REVERSE, identity.xp_reverse);
-    UPDATE_BLE_CHARACTERISTIC(IDENTITY_CHAR_UUID_XP_CRYPTO, identity.xp_crypto);
-    UPDATE_BLE_CHARACTERISTIC(IDENTITY_CHAR_UUID_XP_WEB, identity.xp_web);
     UPDATE_BLE_CHARACTERISTIC(IDENTITY_CHAR_UUID_AVATAR, identity.avatar);
     UPDATE_BLE_CHARACTERISTIC(IDENTITY_CHAR_UUID_IS_UNLOCKED, identity.unlocked);
 }
@@ -118,10 +92,12 @@ void nsec_identitiy_init(void) {
 void nsec_identity_draw(void) {
     gfx_fillRect(8, 8, 128-16, 64-16, BLACK);
     gfx_drawBitmapBg(8, 16, identity.avatar, NSEC_IDENTITY_AVATAR_WIDTH, NSEC_IDENTITY_AVATAR_HEIGHT, WHITE, BLACK);
-    gfx_setCursor(16+16+8, 16);
-    gfx_puts(identity.name);
+    gfx_setCursor(16+16+8, 20);
+    char name_with_spaces[sizeof(identity.name)];
+    snprintf(name_with_spaces, sizeof(name_with_spaces), "%-14s", identity.name);
+    gfx_puts(name_with_spaces);
     // TODO: Add progress bar
-    gfx_update();
+    //gfx_update();
 }
 
 void nsec_identity_get_unlock_key(char * data, size_t length) {
@@ -134,15 +110,17 @@ static void nsec_identity_ble_callback(nsec_ble_service_handle service, uint16_t
             if(identity.unlocked) {
                 memset(identity.name, 0, sizeof(identity.name));
                 strncpy(identity.name, (char *) content,
-                        MIN(content_length + 1, sizeof(identity.name)));
+                        MIN(content_length, sizeof(identity.name)));
                 UPDATE_BLE_CHARACTERISTIC(IDENTITY_CHAR_UUID_NAME, identity.name);
             }
+            nsec_identity_draw();
         }
         case IDENTITY_CHAR_UUID_AVATAR: {
             if(identity.unlocked && content_length == AVATAR_SIZE) {
                 memcpy(identity.avatar, content, AVATAR_SIZE);
                 UPDATE_BLE_CHARACTERISTIC(IDENTITY_CHAR_UUID_AVATAR, identity.avatar);
             }
+            nsec_identity_draw();
         }
         case IDENTITY_CHAR_UUID_UNLOCK_KEY: {
             char key[8];
