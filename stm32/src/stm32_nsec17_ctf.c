a4f085b6c6fc2c92ff6f8d6d368b664b01428156
benjamin vanheuverzwijn <bvanheu@gmail.com>
2017-05-19 12:48:05 -0400

    Added read protection

--- stm32/src/stm32_nsec17_ctf.c
+++ stm32/src/stm32_nsec17_ctf.c
@@ -4,10 +4,10 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/flash.h>
 
 #include "tsc_n.h"
 #include "touch_handler.h"
-#include "usart.h"
 #include "shell.h"
 #include "scheduler.h"
 #include "usb_cdc_acm.h"
@@ -19,8 +19,23 @@ static void clock_setup(void) {
     rcc_periph_clock_enable(RCC_GPIOB);
 }
 
+void enable_read_protect(void);
+void enable_read_protect(void) {
+    if ((FLASH_OBR & FLASH_OBR_RDPRT) != FLASH_OBR_RDPRT_L1) {
+        flash_unlock();
+        flash_erase_option_bytes();
+        flash_program_option_bytes(0x1ffff800, 0x55f0);
+        FLASH_CR |= FLASH_CR_OBL_LAUNCH;
+        flash_lock();
+        // Wait for system reboot
+        while (true);
+    }
+}
+
 int main(void) {
     clock_setup();
+    enable_read_protect();
+
     touch_init();
     scheduler_init();
 
