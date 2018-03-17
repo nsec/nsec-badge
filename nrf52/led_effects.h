1153786a89888603b0716dc4d0e8af3b8c2060fb
Eric Tremblay <etremblay@nsec.io>
2018-03-17 16:21:05 -0400

    Implement DWT for controlling the neoPixel
    PWM is not working right now, the performance should be better with PWM but DWT is working

--- nrf52/led_effects.h
+++ nrf52/led_effects.h
@@ -6,21 +6,17 @@
 #ifndef neoPixel_h
 #define neoPixel_h
 
-#define NEO_GRB  	82 //((1 << 6) | (1 << 4) | (0 << 2) | (2))
+#define NEO_GRB  	((1 << 6) | (1 << 4) | (0 << 2) | (2))
 #define MAGIC_T0H  	6UL | (0x8000) // 0.375us
 #define MAGIC_T1H  	13UL | (0x8000) // 0.8125us
 #define CTOPVAL 	20UL // 1.25us
 
+#define CYCLES_800_T0H	18 // ~0.36 us
+#define CYCLES_800_T1H	41 // ~0.76 us
+#define CYCLES_800 		71 // ~1.25 us
+
 #define NEOPIXEL_COUNT	8
 #define LED_PIN			26
-
-struct nsec18_pixels {
-	uint16_t numBytes;
-	uint8_t rOffset;
-	uint8_t gOffset;
-	uint8_t bOffset;
-	uint8_t *pixels;
-};
 
 int nsec_neopixel_init(void);
 void nsec_neoPixel_clean(void);
