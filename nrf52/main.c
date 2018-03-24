24fa9c386abf32a6797de3d7b3e32690ca5a6a70
Eric Tremblay <etremblay@nsec.io>
2018-03-24 10:17:31 -0400

    Move application timer into is own module

--- nrf52/main.c
+++ nrf52/main.c
@@ -23,10 +23,7 @@
 
 #include "nrf_drv_power.h"
 
-#include "app_timer.h"
-#include "nrf_drv_clock.h"
-
-#define APP_TIMER_TIMEOUT     500
+#include "timer.h"
 
 void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info){
 	NRF_LOG_ERROR("An error happened");
@@ -58,41 +55,6 @@ void init_DCDC(void) {
      * POWER_CONFIG_DEFAULT_DCDCEN from sdk_config.h
      */
     err_code = nrf_drv_power_init(NULL);
-    APP_ERROR_CHECK(err_code);
-}
-
-/*
- * Task timers
- */
-APP_TIMER_DEF(m_heartbeat_timer_id);
-static void heartbeat_timeout_handler(void *p_context) {
-    // TODO notify ws2812fx
-}
-
-/*
- * Initialize the app timer library and heartbeat
- */
-static void timers_init(void) {
-    ret_code_t err_code;
-
-    // Initialize the application timer module
-    err_code = app_timer_init();
-    APP_ERROR_CHECK(err_code);
-
-    err_code = app_timer_create(&m_heartbeat_timer_id ,
-                APP_TIMER_MODE_REPEATED,
-                heartbeat_timeout_handler);
-
-    APP_ERROR_CHECK(err_code);
-}
-
-static void application_timers_start(void) {
-    ret_code_t err_code;
-
-    // Start application timers.
-    //TODO confirm if 500ms is ok
-    err_code = app_timer_start(m_heartbeat_timer_id,
-                         APP_TIMER_TICKS(APP_TIMER_TIMEOUT /* ms */), NULL);
     APP_ERROR_CHECK(err_code);
 }
 
---
+++ nrf52/timer.h
@@ -0,0 +1,7 @@
+//  Copyright (c) 2018
+//  Eric Tremblay <habscup@gmail.com>
+//
+//  License: MIT (see LICENSE for details)
+
+void timers_init(void);
+void application_timers_start(void);
