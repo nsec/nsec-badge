a27f98962e91c4011c7464b2f6b58963e499d337
Eric Tremblay <etremblay@nsec.io>
2018-03-23 20:27:29 -0400

    Add app_timer and heartbeat function
    Remove test_neopixel

--- nrf52/main.c
+++ nrf52/main.c
@@ -23,6 +23,11 @@
 
 #include "nrf_drv_power.h"
 
+#include "app_timer.h"
+#include "nrf_drv_clock.h"
+
+#define APP_TIMER_TIMEOUT     500
+
 void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info){
 	NRF_LOG_ERROR("An error happened");
 }
@@ -31,16 +36,6 @@ static void softdevice_init(void) {
     uint32_t err_code;
 	err_code = nrf_sdh_enable_request();
 	log_error_code("nrf_sdh_enable_request", err_code);
-}
-
-void test_neopixels(){
-    nsec_neopixel_init();
-	
-	for(int i = 0; i < NEOPIXEL_COUNT; i++){
-		nsec_set_pixel_color(i, 0, 0, 50);
-	}
-	while(1)
-		nsec_neopixel_show();
 }
 
 void init_devboard(){
@@ -66,11 +61,48 @@ void init_DCDC(void) {
     APP_ERROR_CHECK(err_code);
 }
 
+/*
+ * Task timers
+ */
+APP_TIMER_DEF(m_heartbeat_timer_id);
+static void heartbeat_timeout_handler(void *p_context) {
+    // TODO notify ws2812fx
+}
+
+/*
+ * Initialize the app timer library and heartbeat
+ */
+static void timers_init(void) {
+    ret_code_t err_code;
+
+    // Initialize the application timer module
+    err_code = app_timer_init();
+    APP_ERROR_CHECK(err_code);
+
+    err_code = app_timer_create(&m_heartbeat_timer_id ,
+                APP_TIMER_MODE_REPEATED,
+                heartbeat_timeout_handler);
+
+    APP_ERROR_CHECK(err_code);
+}
+
+static void application_timers_start(void) {
+    ret_code_t err_code;
+
+    // Start application timers.
+    //TODO confirm if 500ms is ok
+    err_code = app_timer_start(m_heartbeat_timer_id,
+                         APP_TIMER_TICKS(APP_TIMER_TIMEOUT /* ms */), NULL);
+    APP_ERROR_CHECK(err_code);
+}
+
 
 int main() {
 	init_devboard();
     init_DCDC();
     softdevice_init();
+    timers_init();
+    application_timers_start();
     create_ble_device("My BLE device");
     configure_advertising();
     config_dummy_service();
