d2da830d2890ff78cf8b6f15bf8470c5699e35ef
Michael Jeanson <mjeanson@gmail.com>
2018-04-03 12:13:11 -0400

    Add hardware initialization to main

--- nrf52/main.c
+++ nrf52/main.c
@@ -16,28 +16,48 @@
 #include <nordic_common.h>
 #include <stdint.h>
 
-#include "led_effects.h"
 #include "logs.h"
 #include "ble/ble_device.h"
 
+#include "buttons.h"
+#include "led_effects.h"
+#include "logs.h"
 #include "timer.h"
 #include "power.h"
 #include "softdevice.h"
+#include "ssd1306.h"
 
 void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info){
 	NRF_LOG_ERROR("An error happened");
 }
 
-int main() {
+int main(void) {
+    /*
+     * Initialize base hardware
+     */
+    log_init();
     power_init();
     softdevice_init();
     timer_init();
+    nsec_neoPixel_init();
+    ssd1306_init();
+    gfx_setTextBackgroundColor(WHITE, BLACK);
+    buttons_init();
+
+    /*
+     * Initialize bluetooth stack
+     */
     create_ble_device("My BLE device");
     configure_advertising();
     config_dummy_service();
     start_advertising();
-    while(1){
+
+    /*
+     * Main loop
+     */
+    while(1) {
     	nrf_delay_ms(500);
     }
+
     return 0;
 }
