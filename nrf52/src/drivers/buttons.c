5d11ec29fbf88c850a91f37848807d962467a936
Simon Marchi <simon.marchi@polymtl.ca>
2019-02-15 00:20:41 -0500

    Bunch of changes to uart API
      - The function that sends a null-terminated string is renamed to
      uart_puts, kind of standard.
    
      - uart_send now takes a pointer and length, so is more versatile than
      just null-terminated strings.
    
      - Introduce uart_printf, which does as expected.
    
      - Introduce uart_read, which reads one byte from the uart, in a
      blocking way.

--- nrf52/src/drivers/buttons.c
+++ nrf52/src/drivers/buttons.c
@@ -43,22 +43,22 @@ void nsec_button_event_handler(uint8_t pin_no, uint8_t button_action)
     if (button_action == APP_BUTTON_PUSH) {
         switch (pin_no) {
             case PIN_INPUT_UP:
-            	uart_send("+ btn up\n");
+                uart_puts("+ btn up\n");
                 nsec_controls_trigger(BUTTON_UP);
             break;
 
             case PIN_INPUT_DOWN:
-            	uart_send("+ btn down\n");
+                uart_puts("+ btn down\n");
                 nsec_controls_trigger(BUTTON_DOWN);
             break;
 
             case PIN_INPUT_BACK:
-            	uart_send("+ btn back\n");
+                uart_puts("+ btn back\n");
                 nsec_controls_trigger(BUTTON_BACK);
             break;
 
             case PIN_INPUT_ENTER:
-            	uart_send("+ btn enter\n");
+                uart_puts("+ btn enter\n");
                 nsec_controls_trigger(BUTTON_ENTER);
             break;
         }
