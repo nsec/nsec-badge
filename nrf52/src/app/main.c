3cbb17a997acb5a4699bfea8bb6123fd8508f410
NicolasAubry <nicolas.aubry@hotmail.com>
2019-03-30 12:48:21 -0400

    Add flag in Makefile for the bar beacon

--- nrf52/src/app/main.c
+++ nrf52/src/app/main.c
@@ -116,6 +116,18 @@ void nsec_intro(void) {
     }
 }
 
+static void init_ble() {
+    create_ble_device(g_device_id);
+#ifdef NSEC_FLAVOR_BAR_BEACON
+    init_resistance_bar_beacon();
+    set_advertiser(get_resistance_bar_beacon());
+    start_advertising();
+#else
+    add_observer(get_resistance_propaganda_observer());
+    ble_device_start_scan();
+#endif
+}
+
 int main(void) {
 #if defined(NSEC_HARDCODED_BLE_DEVICE_ID)
     sprintf(g_device_id, "%.8s", NSEC_STRINGIFY(NSEC_HARDCODED_BLE_DEVICE_ID));
@@ -149,16 +161,7 @@ int main(void) {
     // (the CLI takes over the UART, which the flash mode uses).
     cli_init();
 
-    /*
-     * Initialize bluetooth stack
-     */
-    create_ble_device(g_device_id);
-    //init_resistance_bar_beacon();
-    //set_advertiser(get_resistance_bar_beacon());
-    //start_advertising();
-    add_observer(get_resistance_propaganda_observer());
-    ble_device_start_scan();
-
+    init_ble();
     nsec_status_bar_init();
     nsec_battery_manager_init();
     //nsec_status_set_name(g_device_id);
