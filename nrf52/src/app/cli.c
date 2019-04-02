8d3fbab4bae11c539f36fd9dadea2a2bd77fe732
Simon Marchi <simon.marchi@polymtl.ca>
2019-04-02 00:00:30 -0400

    Implement nsec CLI command
    "nsec" prints the nsec logo, "nsec --help" or "nsec -h" gives a flag.

--- nrf52/src/app/cli.c
+++ nrf52/src/app/cli.c
@@ -24,22 +24,48 @@
 
 #include <drivers/cli_uart.h>
 
-/* The "prout" command. */
+static struct {
+    const char *n_line, *sec_line;
+} nsec_rows[] = {
+    // clang-format off
+    {"                       ", "                                        `/ydmmmdy+.      "},
+    {"                       ", "                                       /mNNmhyhmNNNy`    "},
+    {"                       ", "                                      :NNN+`    -dNNh    "},
+    {"     -ohy  `:+oo+/.    ", "  -+yhdmmdhyo/.      .:/+++/:.        /NNd-:/+++/oNNN`   "},
+    {"  :hNMMMh-hMMMMMMMMNo  ", ":dNNNNNNNNNNNNNd. -sdNNNNNNNNNds-     /NNNNNNNNNNNNNm-   "},
+    {"  sMMMMMNMMMMMMMMMMMMd ", "hNNNNmyssydNNNm/.yNNNNNNNNNNNNNNNy` `sNNNNNNNNNNNNNNNm/  "},
+    {"  sMMMMMMMy+/+yNMMMMMM+", " NNNN/     `:o:-mNNNNmo-` `-sNNNNNd:mNNNNNdo:--:+yNms`   "},
+    {"  sMMMMMN.     .NMMMMMy", " mNNNNho/-`   `mNNNNm-       /NNNNNNNNNNNo        ..     "},
+    {"  sMMMMMh       hMMMMMy", " mNNNNNNNNNds/+NNNNNdsssssssssNNNNNNNNNNh                "},
+    {"  sMMMMMh       hMMMMMy", " -odNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNy                "},
+    {"  sMMMMMh       hMMMMMy", "    `-/oydNNNNNNNNNNm:::::::::::::/NNNNNm.               "},
+    {"  sMMMMMh       hMMMMMy", " o:`      .mNNNNNNNNNy.       -o-  sNNNNNm+`     .oy:    "},
+    {"  sMMMMMh       hMMMMMy", " mNmhs+//+yNNNNNdNNNNNNhsoosymNNNh: omNNNNNNdhyhmNNNNh:  "},
+    {"  sMMMMMh       hMMMMMy", " mNNNNNNNNNNNNNy`-ymNNNNNNNNNNNNmy-  .smNNNNNNNNNNNNms.  "},
+    {"  +hhhhho       ohhhhho", " /shmNNNNNNmho-    `:oyhdmmmdyo:`      `:oyhdmmddyo:`    "},
+    {"                       ", "      `..``                                              "},
+    // clang-format on
+};
 
-static void do_prout(const nrf_cli_t *p_cli, size_t argc, char **argv) {
-    nrf_cli_fprintf(p_cli, NRF_CLI_VT100_COLOR_MAGENTA,
-                    " ____                  _   _\r\n");
-    nrf_cli_fprintf(p_cli, NRF_CLI_VT100_COLOR_MAGENTA,
-                    "|  _ \\ _ __ ___  _   _| |_| |\r\n");
-    nrf_cli_fprintf(p_cli, NRF_CLI_VT100_COLOR_MAGENTA,
-                    "| |_) | '__/ _ \\| | | | __| |\r\n");
-    nrf_cli_fprintf(p_cli, NRF_CLI_VT100_COLOR_MAGENTA,
-                    "|  __/| | | (_) | |_| | |_|_|\r\n");
-    nrf_cli_fprintf(p_cli, NRF_CLI_VT100_COLOR_MAGENTA,
-                    "|_|   |_|  \\___/ \\__,_|\\__(_)\r\n");
+static void do_nsec(const nrf_cli_t *p_cli, size_t argc, char **argv) {
+    if (nrf_cli_help_requested(p_cli)) {
+
+        nrf_cli_fprintf(
+            p_cli, NRF_CLI_VT100_COLOR_DEFAULT,
+            "It looks like you could use some help, here's a flag!\r\n\r\n");
+        nrf_cli_fprintf(p_cli, NRF_CLI_VT100_COLOR_YELLOW,
+                        "         173f42fc-ade7-4d5e-ac0a-915f180fcb2b\r\n");
+        return;
+    }
+
+    for (int i = 0; i < ARRAY_SIZE(nsec_rows); i++) {
+        nrf_fprintf(p_cli->p_fprintf_ctx, "\x1b[38;2;19;118;188m");
+        nrf_fprintf(p_cli->p_fprintf_ctx, "%s", nsec_rows[i].n_line);
+        nrf_fprintf(p_cli->p_fprintf_ctx, "\x1b[38;2;255;255;255m");
+        nrf_fprintf(p_cli->p_fprintf_ctx, "%s\r\n", nsec_rows[i].sec_line);
+    }
 }
-
-NRF_CLI_CMD_REGISTER(prout, NULL, "Does prout.", do_prout);
+NRF_CLI_CMD_REGISTER(nsec, NULL, "Welcome to NSEC!", do_nsec);
 
 /* Initialize the command-line interface module.  */
 
