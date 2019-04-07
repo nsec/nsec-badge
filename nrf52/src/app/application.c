688fea9d5381b81280701b0769236c867e4e01cc
20th <1331328+20th@users.noreply.github.com>
2019-04-07 03:30:35 +0000

    Use home menu as a fallback application

--- nrf52/src/app/application.c
+++ nrf52/src/app/application.c
@@ -1,9 +1,12 @@
 #include "application.h"
+#include "home_menu.h"
 
 static application_t application = 0;
 
 void application_clear() { application = 0; }
 
-application_t application_get() { return application; }
+application_t application_get() {
+    return application ? application : home_menu_application;
+}
 
 void application_set(application_t callback) { application = callback; }
