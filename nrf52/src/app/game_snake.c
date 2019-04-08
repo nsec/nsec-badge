8041bcd970e29216492d6795f7752b2ce60b2146
20th <1331328+20th@users.noreply.github.com>
2019-04-08 01:31:04 +0000

    Play snake game splash screen animation

--- nrf52/src/app/game_snake.c
+++ nrf52/src/app/game_snake.c
@@ -1,8 +1,35 @@
 #include "drivers/controls.h"
+#include "drivers/display.h"
 
 #include "application.h"
+#include "gfx_effect.h"
+
+#include "images/external/snake_splash_10_bitmap.h"
+#include "images/external/snake_splash_11_bitmap.h"
+#include "images/external/snake_splash_12_bitmap.h"
+#include "images/external/snake_splash_13_bitmap.h"
+#include "images/external/snake_splash_14_bitmap.h"
+#include "images/external/snake_splash_15_bitmap.h"
+#include "images/external/snake_splash_1_bitmap.h"
+#include "images/external/snake_splash_2_bitmap.h"
+#include "images/external/snake_splash_3_bitmap.h"
+#include "images/external/snake_splash_4_bitmap.h"
+#include "images/external/snake_splash_5_bitmap.h"
+#include "images/external/snake_splash_6_bitmap.h"
+#include "images/external/snake_splash_7_bitmap.h"
+#include "images/external/snake_splash_8_bitmap.h"
+#include "images/external/snake_splash_9_bitmap.h"
+#include "images/external/snake_splash_bitmap.h"
 
 #define SNAKE_BUTTON_NONE 255
+
+static const struct bitmap_ext *splash_animation[] = {
+    &snake_splash_1_bitmap,  &snake_splash_2_bitmap,  &snake_splash_3_bitmap,
+    &snake_splash_4_bitmap,  &snake_splash_5_bitmap,  &snake_splash_6_bitmap,
+    &snake_splash_7_bitmap,  &snake_splash_8_bitmap,  &snake_splash_9_bitmap,
+    &snake_splash_10_bitmap, &snake_splash_11_bitmap, &snake_splash_12_bitmap,
+    &snake_splash_13_bitmap, &snake_splash_14_bitmap, &snake_splash_15_bitmap,
+};
 
 static uint8_t snake_button_read_value = SNAKE_BUTTON_NONE;
 
@@ -33,9 +60,34 @@ static uint8_t snake_buttons_read()
     return SNAKE_BUTTON_NONE;
 }
 
+static void snake_boot_sequence(void (*service_device)())
+{
+    service_device();
+    display_draw_16bit_ext_bitmap(0, 0, &snake_splash_bitmap, 0);
+
+    snake_buttons_read();
+    do {
+        service_device();
+    } while (snake_buttons_read() == SNAKE_BUTTON_NONE);
+
+    for (uint8_t i = 0; i < 15; i++) {
+        service_device();
+        display_draw_16bit_ext_bitmap(40, 0, splash_animation[i], 0);
+    }
+
+    do {
+        service_device();
+    } while (snake_buttons_read() == SNAKE_BUTTON_NONE);
+
+    gfx_fill_rect(0, 0, DISPLAY_HEIGHT, DISPLAY_WIDTH, DISPLAY_BLACK);
+    gfx_update();
+}
+
 void snake_application(void (*service_device)())
 {
     nsec_controls_add_handler(snake_buttons_handle);
+
+    snake_boot_sequence(service_device);
 
     while (application_get() == snake_application) {
         service_device();
