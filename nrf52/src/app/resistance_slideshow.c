90f6382ddaa1fa323f86a036fbec382728b1786c
20th <1331328+20th@users.noreply.github.com>
2019-04-22 22:37:26 +0000

    Add a very simple static noice to the resistance slideshow

--- nrf52/src/app/resistance_slideshow.c
+++ nrf52/src/app/resistance_slideshow.c
@@ -1,4 +1,5 @@
 #include "drivers/display.h"
+#include "random.h"
 
 #include "application.h"
 
@@ -10,6 +11,18 @@
 #include "images/external/ctf/resistance_slideshow_3_bitmap.h"
 #include "images/external/ctf/resistance_slideshow_4_bitmap.h"
 
+static void resistance_slideshow_add_static(uint8_t count)
+{
+    uint8_t x, y;
+
+    for (uint8_t i = 0; i < count; i++) {
+        x = nsec_random_get_byte(DISPLAY_WIDTH);
+        y = nsec_random_get_byte(DISPLAY_HEIGHT);
+
+        display_draw_pixel(y, x, 0x9c99);
+    }
+}
+
 void resistance_slideshow_app(void (*service_device)())
 {
     uint16_t step = 0;
@@ -18,11 +31,15 @@ void resistance_slideshow_app(void (*service_device)())
         if (step == 0) {
             display_draw_16bit_ext_bitmap(
                 0, 0, &resistance_slideshow_1_base_bitmap, 0);
+
+            resistance_slideshow_add_static(250);
         }
 
         if (step < 720) {
             display_draw_16bit_ext_bitmap(
                 360 - step, 41, &resistance_slideshow_1_overlay_bitmap, 0);
+
+            resistance_slideshow_add_static(5);
         }
 
         if (step == 720) {
@@ -40,6 +57,9 @@ void resistance_slideshow_app(void (*service_device)())
                 0, 0, &resistance_slideshow_4_bitmap, 0);
         }
 
+        if (step > 1600) {
+            resistance_slideshow_add_static(125);
+        }
 
         service_device();
         step++;
