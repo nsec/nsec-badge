fbba52431f7732206dd88cb318c004683114b750
Eric Tremblay <habscup@gmail.com>
2019-05-04 14:54:14 -0400

    MENU: move badge info to main menu

--- nrf52/src/app/main_menu.c
+++ nrf52/src/app/main_menu.c
@@ -16,6 +16,7 @@
 #include "gui.h"
 #include "home_menu.h"
 #include "identity.h"
+#include "persistency.h"
 #include "menu.h"
 #include "nsec_conf_schedule.h"
 #include "nsec_games_menu.h"
@@ -26,11 +27,18 @@
 #include "timer.h"
 #include "utils.h"
 
-enum main_menu_state { MAIN_MENU_STATE_CLOSED, MAIN_MENU_STATE_MENU };
+enum main_menu_state {
+    MAIN_MENU_STATE_CLOSED,
+    MAIN_MENU_STATE_MENU,
+    MAIN_MENU_STATE_BADGE_INFO
+};
 
 static enum main_menu_state _state = MAIN_MENU_STATE_CLOSED;
 
 static void main_handle_buttons(button_t button);
+
+static char identity_string[] = "Id: Citizen #XXXXXXX";
+static char ble_id_string[] = "BLE id: NSECXXXX";
 
 #ifdef NSEC_FLAVOR_CONF
 void open_conference_schedule(uint8_t item) {
@@ -71,6 +79,28 @@ void open_games_menu(uint8_t item)
     nsec_games_menu_show();
 }
 
+static menu_item_s badge_info_items[] = {
+    {
+        .label = identity_string,
+        .handler = NULL,
+    }, {
+        .label = ble_id_string,
+        .handler = NULL,
+    }
+};
+
+static void show_badge_info(uint8_t item)
+{
+    menu_close();
+    gfx_fill_rect(GEN_MENU_POS, GEN_MENU_WIDTH, GEN_MENU_HEIGHT, DISPLAY_WHITE);
+
+    menu_init(GEN_MENU_POS, GEN_MENU_WIDTH, GEN_MENU_HEIGHT,
+              ARRAY_SIZE(badge_info_items), badge_info_items,
+              HOME_MENU_BG_COLOR, DISPLAY_WHITE);
+
+    _state = MAIN_MENU_STATE_BADGE_INFO;
+}
+
 static menu_item_s main_menu_items[] = {
 #ifdef NSEC_FLAVOR_CONF
     {
@@ -87,6 +117,10 @@ static menu_item_s main_menu_items[] = {
         .handler = open_led_pattern,
     },
     {
+        .label = "Badge info",
+        .handler = show_badge_info,
+    },
+    {
         .label = "Battery status",
         .handler = open_battery_status,
     },
@@ -96,6 +130,14 @@ static menu_item_s main_menu_items[] = {
     }};
 
 void show_main_menu(void) {
+#if defined(NSEC_HARDCODED_BLE_DEVICE_ID)
+    sprintf(ble_id_string, "%.8s", NSEC_STRINGIFY(NSEC_HARDCODED_BLE_DEVICE_ID));
+#else
+    sprintf(ble_id_string, "BLE id: NSEC%04X", (uint16_t)(NRF_FICR->DEVICEID[1] % 0xFFFF));
+#endif
+
+    sprintf(identity_string, "Id: %s", get_stored_identity());
+
     draw_main_menu_title();
     menu_init(GEN_MENU_POS, GEN_MENU_WIDTH, GEN_MENU_HEIGHT,
               ARRAY_SIZE(main_menu_items), main_menu_items, HOME_MENU_BG_COLOR,
@@ -112,7 +154,10 @@ static void main_handle_buttons(button_t button) {
             menu_close();
             show_home_menu(HOME_STATE_MENU);
             break;
-
+        case MAIN_MENU_STATE_BADGE_INFO:
+            _state = MAIN_MENU_STATE_MENU;
+            show_main_menu();
+            break;
         default:
             break;
         }
--- nrf52/src/app/nsec_settings.c
+++ nrf52/src/app/nsec_settings.c
@@ -7,6 +7,7 @@
 #include <nrf.h>
 #include <nordic_common.h>
 #include <stdio.h>
+#include <string.h>
 #include "home_menu.h"
 #include "main_menu.h"
 #include "gui.h"
@@ -19,7 +20,6 @@
 #include "ble/ble_device.h"
 #include "status_bar.h"
 #include "drivers/controls.h"
-#include "identity.h"
 #include "drivers/ws2812fx.h"
 #include "persistency.h"
 #include "timer.h"
@@ -45,16 +45,12 @@ enum setting_state {
     SETTING_STATE_SCREEN_OFF,
     SETTING_STATE_FLASHLIGHT,
     SETTING_STATE_BATTERY,
-    SETTING_CONFIRM_FACTORY,
-    SETTING_STATE_BADGE_INFO
+    SETTING_CONFIRM_FACTORY
 };
 
 static enum setting_state _state = SETTING_STATE_CLOSED;
 
 static void setting_handle_buttons(button_t button);
-
-static char identity_string[] = "Id: Citizen #XXXXXXX";
-static char ble_id_string[] = "BLE id: NSECXXXX";
 
 static menu_item_s settings_items[] = {
     {
@@ -67,9 +63,6 @@ static menu_item_s settings_items[] = {
         .label = "Factory Reset",
         .handler = do_factory_reset,
     }, {
-        .label = "Badge info",
-        .handler = show_badge_info,
-    },{
         .label = "Toggle Bluetooth",
         .handler = toggle_bluetooth,
     }, {
@@ -118,28 +111,6 @@ static menu_item_s confirm_items[] = {
         .handler = confirm_factory_reset,
     }
 };
-
-static menu_item_s badge_info_items[] = {
-    {
-        .label = identity_string,
-        .handler = NULL,
-    }, {
-        .label = ble_id_string,
-        .handler = NULL,
-    }
-};
-
-static void show_badge_info(uint8_t item)
-{
-    menu_close();
-    gfx_fill_rect(GEN_MENU_POS, GEN_MENU_WIDTH, GEN_MENU_HEIGHT, DISPLAY_WHITE);
-
-    menu_init(GEN_MENU_POS, GEN_MENU_WIDTH, GEN_MENU_HEIGHT,
-              ARRAY_SIZE(badge_info_items), badge_info_items,
-              HOME_MENU_BG_COLOR, DISPLAY_WHITE);
-
-    _state = SETTING_STATE_BADGE_INFO;
-}
 
 static void confirm_factory_reset(uint8_t item)
 {
@@ -319,14 +290,6 @@ static void turn_off_screen(uint8_t item) {
 
 void nsec_setting_show(void) {
 
-#if defined(NSEC_HARDCODED_BLE_DEVICE_ID)
-    sprintf(ble_id_string, "%.8s", NSEC_STRINGIFY(NSEC_HARDCODED_BLE_DEVICE_ID));
-#else
-    sprintf(ble_id_string, "BLE id: NSEC%04X", (uint16_t)(NRF_FICR->DEVICEID[1] % 0xFFFF));
-#endif
-
-    sprintf(identity_string, "Id: %s", get_stored_identity());
-
     draw_settings_title();
 
     menu_init(GEN_MENU_POS, GEN_MENU_WIDTH, GEN_MENU_HEIGHT,
@@ -371,7 +334,6 @@ static void setting_handle_buttons(button_t button)
                 display_set_brightness(get_stored_display_brightness());
                 // no break
             case SETTING_STATE_CREDIT:
-            case SETTING_STATE_BADGE_INFO:
                 nsec_setting_show();
                 break;
 
