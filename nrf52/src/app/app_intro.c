d958ab1ccefff82cc7a079ac25de70e089f86d81
Eric Tremblay <habscup@gmail.com>
2019-05-04 14:08:49 -0400

    INTRO: Service LED and CLI during animation

--- nrf52/src/app/app_intro.c
+++ nrf52/src/app/app_intro.c
@@ -5,6 +5,8 @@
 
 #include "drivers/controls.h"
 #include "drivers/display.h"
+#include "drivers/ws2812fx.h"
+#include "cli.h"
 #include "gfx_effect.h"
 #include "gui.h"
 #include "main_menu.h"
@@ -12,6 +14,7 @@
 #include "nrf_delay.h"
 #include "app_intro.h"
 #include "persistency.h"
+#include "timer.h"
 
 #include "images/neurosoft_logo_bitmap.h"
 #include "images/neurosoft_circle_bitmap.h"
@@ -45,6 +48,28 @@ static struct line brain[15] = {
     {47, 43, 6}
 };
 
+static void service_process(void)
+{
+    cli_process();
+    service_WS2812FX();
+}
+
+static void service_delay_ms(uint32_t ms)
+{
+    if (ms <= 50) {
+        service_process();
+        nrf_delay_ms(ms);
+        service_process();
+    } else {
+        int64_t remaining = ms;
+        do {
+            uint64_t start = get_current_time_millis();
+            service_process();
+            remaining -= get_current_time_millis() - start;
+        } while (remaining > 0);
+    }
+}
+
 static void animated_brain(void)
 {
     uint16_t gfx_width = gfx_get_screen_width();
@@ -62,7 +87,7 @@ static void animated_brain(void)
         // Start by drawing a white square where we want to draw the real pixel
         for (int x = start_x; x < len + start_x; x+=3) {
             gfx_fill_rect(x + offset_x, start_y + offset_y, 3, 3, DISPLAY_WHITE);
-            nrf_delay_ms(10);
+            service_delay_ms(10);
 
             // Draw the 9 pixels
             for (int l = 0; l < 3; l++) {
@@ -75,7 +100,7 @@ static void animated_brain(void)
                         color);
                 }
             }
-            nrf_delay_ms(10);
+            service_delay_ms(10);
         }
     }
 }
@@ -89,7 +114,7 @@ static void animated_name(const struct bitmap *bitmap)
 
     for (int x = 0; x < gfx_width; x += 4) {
         gfx_fill_rect(x, offset_y, 4, 10, DISPLAY_WHITE);
-        nrf_delay_ms(30);
+        service_delay_ms(30);
         if (x < offset_x || x >= (offset_x + bitmap->width)) {
             gfx_fill_rect(x, offset_y, 4, 10, DISPLAY_BLACK);
         } else {
@@ -109,11 +134,11 @@ static void animated_name(const struct bitmap *bitmap)
 static void greetings(void)
 {
     gfx_draw_16bit_bitmap(12, 25, &avatar_neuro_bitmap, DISPLAY_BLACK);
-    nrf_delay_ms(500);
+    service_delay_ms(500);
     gfx_set_text_background_color(DISPLAY_WHITE, DISPLAY_BLACK);
     gfx_set_cursor(63, 30);
     gfx_puts_lag("Greetings,", 20);
-    nrf_delay_ms(500);
+    service_delay_ms(500);
     gfx_set_cursor(63, 42);
     gfx_puts_lag(get_stored_identity(), 20);
 }
@@ -123,20 +148,20 @@ static void greetings(void)
 void app_intro(void (*service_callback)())
 {
     gfx_draw_16bit_bitmap(47, 22, &neurosoft_circle_bitmap, DISPLAY_BLACK);
-    nrf_delay_ms(500);
+    service_delay_ms(500);
 
     animated_brain();
-    nrf_delay_ms(100);
+    service_delay_ms(100);
 
     animated_name(&neurosoft_name_bitmap);
-    nrf_delay_ms(100);
+    service_delay_ms(100);
 
     animated_name(&beyond_reality_bitmap);
-    nrf_delay_ms(100);
+    service_delay_ms(100);
 
     display_fill_screen_black();
     greetings();
-    nrf_delay_ms(3000);
+    service_delay_ms(3000);
 
     /* Return to default app */
     application_clear();
@@ -145,7 +170,7 @@ void app_intro(void (*service_callback)())
 void app_intro(void (*service_callback)())
 {
     greetings();
-    nrf_delay_ms(1000);
+    service_delay_ms(1000);
 
     /* Return to default app */
     application_clear();
