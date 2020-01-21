8f9fc0ee4a0d673d53be7324bfb1d86e7419da43
Michael Jeanson <mjeanson@gmail.com>
2020-01-20 21:18:11 -0500

    Download nRF5 sdk v16.0.0
    Signed-off-by: Michael Jeanson <mjeanson@gmail.com>

--- nrf52/utils/install-sdk.sh
+++ nrf52/utils/install-sdk.sh
@@ -5,7 +5,12 @@ set -e
 SCRIPTPATH=$(dirname $0)
 
 SDK_DIR=$1
-SDK_VER=$2
+SDK_MAJOR_VER=$2
+SDK_MINOR_VER=$3
+SDK_PATCH_VER=$4
+SDK_COMMIT_VER=$5
+
+SDK_VER="nRF5_SDK_${SDK_MAJOR_VER}.${SDK_MINOR_VER}.${SDK_PATCH_VER}_${SDK_COMMIT_VER}"
 
 GNU_PREFIX=arm-none-eabi
 
@@ -20,33 +25,34 @@ if test -d "${SDK_VER}" || test -d "${SDK_DIR}"; then
     rm -rf "${SDK_VER}" "${SDK_DIR}"
 fi
 
-SDK_MAJOR="$(echo "${SDK_VER}" | sed -E 's/^nRF5_SDK_([0-9]+).*/\1/')"
+mkdir "${SDK_DIR}"
 
-wget -c "http://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v${SDK_MAJOR}.x.x/${SDK_VER}.zip"
+wget -c "http://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v${SDK_MAJOR_VER}.x.x/${SDK_VER}.zip"
 
-unzip -n "${SDK_VER}.zip" "${SDK_VER}/components/*" \
-	"${SDK_VER}/external/*" \
-	"${SDK_VER}/external_tools/cmsisconfig/CMSIS_Configuration_Wizard.jar" \
-	"${SDK_VER}/documentation/nRF5_Nordic_license.txt" \
-	"${SDK_VER}/documentation/nRF5_Dynastream_license.txt"
+unzip -n "${SDK_VER}.zip" -d "${SDK_VER}" \
+	"components/*" \
+	"modules/*" \
+	"external/*" \
+	"external_tools/cmsisconfig/CMSIS_Configuration_Wizard.jar" \
+	"documentation/nRF5_Nordic_license.txt"
 
-mv "${SDK_VER}/components" "${SDK_DIR}"
+mv "${SDK_VER}/components" "${SDK_DIR}/"
+mv "${SDK_VER}/modules" "${SDK_DIR}/"
 mv "${SDK_VER}/external" "${SDK_DIR}/"
 mv "${SDK_VER}/external_tools" "${SDK_DIR}/"
 mv "${SDK_VER}/documentation/nRF5_Nordic_license.txt" "${SDK_DIR}/"
-mv "${SDK_VER}/documentation/nRF5_Dynastream_license.txt" "${SDK_DIR}/"
-cp "${SDK_DIR}/softdevice/s132/doc/s132_nrf52_5.0.0_licence-agreement.txt" "${SDK_DIR}/"
+cp "${SDK_DIR}/components/softdevice/s132/doc/s132_nrf52_7.0.1_licence-agreement.txt" "${SDK_DIR}/"
 
 echo "GNU_PREFIX ?= ${GNU_PREFIX}
 GNU_INSTALL_ROOT ?= \$(shell dirname \$(shell which \$(GNU_PREFIX)-gcc))/
-GNU_VERSION ?= \$(shell \"\$(GNU_INSTALL_ROOT)\$(GNU_PREFIX)-gcc\" -dumpversion)" > "${SDK_DIR}/toolchain/gcc/Makefile.posix"
+GNU_VERSION ?= \$(shell \"\$(GNU_INSTALL_ROOT)\$(GNU_PREFIX)-gcc\" -dumpversion)" > "${SDK_DIR}/components/toolchain/gcc/Makefile.posix"
 
 # Patch
-patch -p0 --binary < "$SCRIPTPATH/../patches/fix_fstorage_init.patch"
-patch -p0 --binary < "$SCRIPTPATH/../patches/fix-ringbuf.patch"
-patch -p0 --binary < "$SCRIPTPATH/../patches/fix-uart-error-handling.patch"
-patch -p0 --binary < "$SCRIPTPATH/../patches/fix-uart-cli-overrun-handling.patch"
-patch -p0 --binary < "$SCRIPTPATH/../patches/cli-make-backspace-and-delete-work.patch"
+#patch -p0 --binary < "$SCRIPTPATH/../patches/fix_fstorage_init.patch"
+#patch -p0 --binary < "$SCRIPTPATH/../patches/fix-ringbuf.patch"
+#patch -p0 --binary < "$SCRIPTPATH/../patches/fix-uart-error-handling.patch"
+#patch -p0 --binary < "$SCRIPTPATH/../patches/fix-uart-cli-overrun-handling.patch"
+#patch -p0 --binary < "$SCRIPTPATH/../patches/cli-make-backspace-and-delete-work.patch"
 
 # Cleanup
 rmdir "${SDK_VER}/documentation"
