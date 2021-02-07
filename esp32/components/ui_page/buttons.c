ff4938a7e8cd77eceedac45e71372deafd66a8fe
20th <1331328+20th@users.noreply.github.com>
2021-02-07 17:44:14 -0500

    Switch to using a task instead of interrupts to handle buttons
    The input from the buttons is very noisy, and the interrupts do not
    provide an easy way to know which way the signal has changed.  Using a
    FreeRTOS task allows to use a history based debouncing to product clean
    events at the expense of some higher CPU usage.

--- esp32/components/ui_page/buttons.c
+++ esp32/components/ui_page/buttons.c
@@ -22,6 +22,8 @@
  */
 
 #include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 #include "buttons.h"
 
@@ -33,87 +35,76 @@
  */
 QueueHandle_t button_event_queue = NULL;
 
-/*
- * Delay from a GPIOTE event until a button is reported as pushed (in number of
- * timer ticks).
- */
-#define BUTTON_DETECTION_DELAY 50
+static void nsec_button_event_task(void *arg)
+{
+    button_t button;
+    int level;
 
-/**@brief Function for handling events from the button handler module.
- */
-static void nsec_button_event_handler(void *arg) {
-    button_t btn;
-    uint32_t pin_no = (uint32_t) arg;
+    uint16_t buttons_history[6] = {};
+    uint8_t buttons_pins[] = {PIN_INPUT_BACK, PIN_INPUT_DOWN,  PIN_INPUT_ENTER,
+                              PIN_INPUT_LEFT, PIN_INPUT_RIGHT, PIN_INPUT_UP};
 
-    if (gpio_get_level(pin_no)) {
-        switch (pin_no) {
-        case PIN_INPUT_BACK:
-            btn = BUTTON_BACK;
-            break;
+    while (true) {
+        for (int i = 0; i < 6; ++i) {
+            level = gpio_get_level(buttons_pins[i]);
 
-        case PIN_INPUT_DOWN:
-            btn = BUTTON_DOWN;
-            break;
+            buttons_history[i] = (buttons_history[i] << 1) | level;
+            switch (buttons_history[i] & 0b1111000000011111) {
+            case 0b0000000000011111:
+                buttons_history[i] = 0xffff;
+                level = 1;
+                break;
 
-        case PIN_INPUT_ENTER:
-            btn = BUTTON_ENTER;
-            break;
+            case 0b1111000000000000:
+                buttons_history[i] = 0;
+                level = 0;
+                break;
 
-        case PIN_INPUT_LEFT:
-            btn = BUTTON_LEFT;
-            break;
+            default:
+                continue;
+            }
 
-        case PIN_INPUT_RIGHT:
-            btn = BUTTON_RIGHT;
-            break;
+            switch (buttons_pins[i]) {
+            case PIN_INPUT_BACK:
+                button = level ? BUTTON_BACK : BUTTON_BACK_RELEASE;
+                break;
 
-        case PIN_INPUT_UP:
-            btn = BUTTON_UP;
-            break;
+            case PIN_INPUT_DOWN:
+                button = level ? BUTTON_DOWN : BUTTON_DOWN_RELEASE;
+                break;
 
-        default:
-            /* Should not happen, but just in case. */
-            return;
+            case PIN_INPUT_ENTER:
+                button = level ? BUTTON_ENTER : BUTTON_ENTER_RELEASE;
+                break;
+
+            case PIN_INPUT_LEFT:
+                button = level ? BUTTON_LEFT : BUTTON_LEFT_RELEASE;
+                break;
+
+            case PIN_INPUT_RIGHT:
+                button = level ? BUTTON_RIGHT : BUTTON_RIGHT_RELEASE;
+                break;
+
+            case PIN_INPUT_UP:
+                button = level ? BUTTON_UP : BUTTON_UP_RELEASE;
+                break;
+
+            default:
+                continue;
+            }
+
+            BaseType_t ret = xQueueSendToBack(button_event_queue, &button, 0);
+            if (ret != errQUEUE_FULL) {
+                assert(ret == pdPASS && "Buttons queue push failed.");
+            }
         }
-    } else {
-        switch (pin_no) {
-        case PIN_INPUT_BACK:
-            btn = BUTTON_BACK_RELEASE;
-            break;
 
-        case PIN_INPUT_DOWN:
-            btn = BUTTON_DOWN_RELEASE;
-            break;
-
-        case PIN_INPUT_ENTER:
-            btn = BUTTON_ENTER_RELEASE;
-            break;
-
-        case PIN_INPUT_LEFT:
-            btn = BUTTON_LEFT_RELEASE;
-            break;
-
-        case PIN_INPUT_RIGHT:
-            btn = BUTTON_RIGHT_RELEASE;
-            break;
-
-        case PIN_INPUT_UP:
-            btn = BUTTON_UP_RELEASE;
-            break;
-
-        default:
-            /* Should not happen, but just in case. */
-            return;
-        }
-    }
-
-    BaseType_t ret = xQueueSendToBackFromISR(button_event_queue, &btn, NULL);
-    if (ret != errQUEUE_FULL) {
-        assert(ret == pdPASS && "Buttons queue push failed.");
+        vTaskDelay(1);
     }
 }
 
-void nsec_buttons_init(void) {
+void nsec_buttons_init(void)
+{
     button_event_queue = xQueueCreate(10, sizeof(button_t));
     assert(button_event_queue != NULL && "Failed to create buttons queue.");
 
@@ -125,7 +116,6 @@ void nsec_buttons_init(void) {
     bit_mask |= 1ULL << PIN_INPUT_RIGHT;
     bit_mask |= 1ULL << PIN_INPUT_UP;
 
-
     gpio_config_t io_conf;
     io_conf.intr_type = GPIO_INTR_ANYEDGE;
     io_conf.pin_bit_mask = bit_mask;
@@ -134,16 +124,12 @@ void nsec_buttons_init(void) {
     io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
     gpio_config(&io_conf);
 
-    gpio_install_isr_service(ESP_INTR_FLAG_LOWMED);
-    gpio_isr_handler_add(PIN_INPUT_BACK, nsec_button_event_handler, (void *)PIN_INPUT_BACK);
-    gpio_isr_handler_add(PIN_INPUT_DOWN, nsec_button_event_handler, (void *)PIN_INPUT_DOWN);
-    gpio_isr_handler_add(PIN_INPUT_ENTER, nsec_button_event_handler, (void *)PIN_INPUT_ENTER);
-    gpio_isr_handler_add(PIN_INPUT_LEFT, nsec_button_event_handler, (void *)PIN_INPUT_LEFT);
-    gpio_isr_handler_add(PIN_INPUT_RIGHT, nsec_button_event_handler, (void *)PIN_INPUT_RIGHT);
-    gpio_isr_handler_add(PIN_INPUT_UP, nsec_button_event_handler, (void *)PIN_INPUT_UP);
+    xTaskCreate(nsec_button_event_task, "nsec_button_event_task", 1024, NULL,
+                10, NULL);
 }
 
-bool nsec_button_is_pushed(button_t button) {
+bool nsec_button_is_pushed(button_t button)
+{
     int idx;
     switch (button) {
     case BUTTON_BACK:
