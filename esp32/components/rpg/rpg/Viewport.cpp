33bcdb8f0c0227096f010b084b1f91dae0c5131b
20th <1331328+20th@users.noreply.github.com>
2021-02-21 14:22:16 -0500

    Maintain state of tiles that need a refresh in the Viewport
    Rendering performance can be boosted significantly if, instead of
    redrawing the whole screen every time, the scene updates only parts of
    the screen that may have actually changed since the last rendering.
    
    Screen space occupied by characters is expected to change every time
    because characters are responsible for rendering themselves, they can
    move and implement some animation.

--- esp32/components/rpg/rpg/Viewport.cpp
+++ esp32/components/rpg/rpg/Viewport.cpp
@@ -3,8 +3,22 @@
 namespace rpg
 {
 
-local_coordinates_t Viewport::get_local_coordinates(int scene_x,
-                                                           int scene_y)
+static inline unsigned int refresh_state_tile_offset(int tile_x, int tile_y)
+{
+    return (viewport_prepend_rows + tile_y) *
+               (viewport_prepend_cols + viewport_tiles_width) +
+           viewport_prepend_cols + tile_x;
+}
+
+void Viewport::cache_refresh_state()
+{
+    for (auto &i : *refresh_state)
+        i = false;
+
+    needs_full_refresh = false;
+}
+
+local_coordinates_t Viewport::get_local_coordinates(int scene_x, int scene_y)
 {
     int screen_x = scene_x - x;
     int screen_y = scene_y - y;
@@ -26,7 +40,7 @@ local_coordinates_t Viewport::get_local_coordinates(int scene_x,
 }
 
 tile_coordinates_t Viewport::get_tile_coordinates(int local_tile_x,
-                                                         int local_tile_y)
+                                                  int local_tile_y)
 {
     return {
         .screen_x = (local_tile_x * DISPLAY_TILE_WIDTH),
@@ -47,6 +61,95 @@ void Viewport::move_to_tile(int new_tile_x, int new_tile_y)
         tile_y = new_tile_y;
         y = new_tile_y * DISPLAY_TILE_HEIGHT;
     }
+
+    needs_full_refresh = true;
 }
 
+void Viewport::prime_refresh_state(data::SceneDataReader &data_reader,
+                                   const std::vector<Character *> &characters)
+{
+    if (needs_full_refresh) {
+        for (auto &i : *refresh_state)
+            i = true;
+
+        return;
+    }
+
+    for (auto character_p : characters) {
+        local_coordinates_t coordinates_array[] = {
+            get_local_coordinates(character_p->get_rendered_scene_x(),
+                                  character_p->get_rendered_scene_y()),
+            get_local_coordinates(character_p->get_scene_x(),
+                                  character_p->get_scene_y()),
+        };
+
+        for (auto &coordinates : coordinates_array) {
+            for (int dy = 0, y_limit = coordinates.screen_y +
+                                       character_p->get_height() +
+                                       DISPLAY_TILE_HEIGHT;
+                 coordinates.screen_y < y_limit;
+                 ++dy, coordinates.screen_y += DISPLAY_TILE_HEIGHT) {
+
+                for (int dx = 0, x_limit = coordinates.screen_x +
+                                           character_p->get_width() +
+                                           DISPLAY_TILE_WIDTH;
+                     coordinates.screen_x < x_limit;
+                     ++dx, coordinates.screen_x += DISPLAY_TILE_WIDTH) {
+
+                    prime_refresh_state_tile(data_reader,
+                                             coordinates.local_tile_x + dx,
+                                             coordinates.local_tile_y + dy);
+                }
+            }
+        }
+    }
 }
+
+void Viewport::prime_refresh_state_tile(data::SceneDataReader &data_reader,
+                                        int tile_x, int tile_y)
+{
+    if (tile_x < -viewport_prepend_cols || tile_y < -viewport_prepend_rows)
+        return;
+
+    if (tile_x >= viewport_tiles_width || tile_y >= viewport_tiles_height)
+        return;
+
+    unsigned int offset = refresh_state_tile_offset(tile_x, tile_y);
+    if ((*refresh_state)[offset])
+        return;
+
+    (*refresh_state)[offset] = true;
+
+    data::tilemap_word_t dependency =
+        data_reader.get_dependency(tile_x, tile_y);
+
+    if (dependency != 0) {
+        data::tilemap_dependency_t decoded_dependency =
+            data_reader.decode_dependency(dependency);
+
+        for (int dy = decoded_dependency.backward_y; dy > -1; --dy) {
+            for (int dx = decoded_dependency.backward_x; dx > -1; --dx) {
+                prime_refresh_state_tile(data_reader, tile_x - dx, tile_y - dy);
+            }
+        }
+
+        for (int dy = 0; dy <= decoded_dependency.forward_y; ++dy) {
+            for (int dx = 0; dx <= decoded_dependency.forward_x; ++dx) {
+                prime_refresh_state_tile(data_reader, tile_x + dx, tile_y + dy);
+            }
+        }
+    }
+}
+
+bool Viewport::tile_needs_refresh(int tile_x, int tile_y) const
+{
+    if (tile_x < -viewport_prepend_cols || tile_y < -viewport_prepend_rows)
+        return false;
+
+    if (tile_x >= viewport_tiles_width || tile_y >= viewport_tiles_height)
+        return false;
+
+    return (*refresh_state)[refresh_state_tile_offset(tile_x, tile_y)];
+}
+
+} // namespace rpg
--- esp32/components/rpg/rpg/Viewport.h
+++ esp32/components/rpg/rpg/Viewport.h
@@ -1,11 +1,29 @@
 #pragma once
 
+#include "rpg/Character.h"
+#include "rpg/data/SceneDataReader.h"
+
 extern "C" {
 #include "graphics.h"
 }
 
+#include <array>
+#include <vector>
+
 namespace rpg
 {
+
+constexpr int viewport_width = 240;
+constexpr int viewport_tiles_width = 10;
+constexpr int viewport_height = 210;
+constexpr int viewport_tiles_height = 9;
+
+constexpr int viewport_prepend_cols = 2;
+constexpr int viewport_prepend_rows = 2;
+
+using viewport_refresh_state_t =
+    std::array<bool, ((viewport_prepend_cols + viewport_tiles_width) *
+                      (viewport_prepend_rows + viewport_tiles_height))>;
 
 struct local_coordinates_t {
     int local_tile_x;
@@ -28,22 +46,28 @@ class Viewport
         : scene_width{scene_width}, scene_height{scene_height},
           scene_width_tiles{scene_width / DISPLAY_TILE_WIDTH},
           scene_height_tiles{scene_height / DISPLAY_TILE_HEIGHT}, x{0}, y{0},
-          tile_x{0}, tile_y{0}
+          tile_x{0}, tile_y{0}, needs_full_refresh{true}
     {
+        refresh_state = new viewport_refresh_state_t();
     }
 
-    // FIXME:
-    // error: 'CONFIG_HEIGHT' was not declared in this scope
-    // note: in expansion of macro 'DISPLAY_PHY_HEIGHT'
-    // note: in expansion of macro 'DISPLAY_WIDTH'
-    static const int width = 240;
-    static const int width_tiles = 10;
-    static const int height = 210;
-    static const int height_tiles = 9;
+    ~Viewport()
+    {
+        delete[] refresh_state;
+    }
+
+    void cache_refresh_state();
 
     local_coordinates_t get_local_coordinates(int scene_x, int scene_y);
+
     tile_coordinates_t get_tile_coordinates(int local_tile_x, int local_tile_y);
+
     void move_to_tile(int new_tile_x, int new_tile_y);
+
+    void prime_refresh_state(data::SceneDataReader &data_reader,
+                             const std::vector<Character *> &characters);
+
+    bool tile_needs_refresh(int tile_x, int tile_y) const;
 
   private:
     const int scene_width;
@@ -54,6 +78,11 @@ class Viewport
     int y;
     int tile_x;
     int tile_y;
+    bool needs_full_refresh;
+    viewport_refresh_state_t *refresh_state;
+
+    void prime_refresh_state_tile(data::SceneDataReader &data_reader,
+                                  int tile_x, int tile_y);
 };
 
 } // namespace rpg
