7b99137f7e8fa1a763ec46c129de19b111061040
Thomas Dupuy <thom4s.d@gmail.com>
2021-02-21 14:55:23 -0500

    Add buzzer component.

---
+++ esp32/components/buzzer/buzzer.c
@@ -0,0 +1,211 @@
+#include "buzzer.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define PIN_BUZZER 26
+
+
+const static playback_t music0[] = {
+    {NOTE_LA3, Q},    {NOTE_LA3, Q},     {NOTE_LA3, Q},
+    {NOTE_F3, E + S}, {NOTE_C4, S},
+
+    {NOTE_LA3, Q},    {NOTE_F3, E + S},  {NOTE_C4, S},
+    {NOTE_LA3, H},
+
+    {NOTE_E4, Q},     {NOTE_E4, Q},      {NOTE_E4, Q},
+    {NOTE_F4, E + S}, {NOTE_C4, S},
+
+    {NOTE_Ab3, Q},    {NOTE_F3, E + S},  {NOTE_C4, S},
+    {NOTE_LA3, H},
+
+    {NOTE_LA4, Q},    {NOTE_LA3, E + S}, {NOTE_LA3, S},
+    {NOTE_LA4, Q},    {NOTE_Ab4, E + S}, {NOTE_G4, S},
+
+    {NOTE_Gb4, S},    {NOTE_E4, S},      {NOTE_F4, E},
+
+    {0, E},           {NOTE_F3, E},      {NOTE_Ab3, Q},
+    {NOTE_F3, E + S}, {NOTE_LA3, S},
+
+    {NOTE_C4, Q},     {NOTE_LA3, E + S}, {NOTE_C4, S},
+    {NOTE_E4, H},
+
+    {NOTE_LA4, Q},    {NOTE_LA3, E + S}, {NOTE_LA3, S},
+    {NOTE_LA4, Q},    {NOTE_Ab4, E + S}, {NOTE_G4, S},
+
+    {NOTE_Gb4, S},    {NOTE_E4, S},      {NOTE_F4, E},
+    {0, E},           {NOTE_Bb3, E},     {NOTE_Eb4, Q},
+    {NOTE_D4, E + S}, {NOTE_Db4, S},
+
+    {NOTE_C4, S},     {NOTE_B3, S},      {NOTE_C4, E},
+    {0, E},           {NOTE_F3, E},      {NOTE_Ab3, Q},
+    {NOTE_F3, E + S}, {NOTE_C4, S},
+
+    {NOTE_LA3, Q},    {NOTE_F3, E + S},  {NOTE_C4, S},
+    {NOTE_LA3, H},    {0, 2 * E}};
+
+// https://onlinesequencer.net/1675803#t0
+const static playback_t music1[] = {
+    {NOTE_Bb4, E},
+    {NOTE_Bb4, E},
+    {NOTE_Bb4, E},
+    {NOTE_Bb4, E},
+
+    {NOTE_D5, E},
+    {NOTE_D5, E},
+    {NOTE_D5, E},
+    {NOTE_D5, E},
+
+    {NOTE_C5, E},
+    {NOTE_C5, E},
+    {NOTE_C5, E},
+    {NOTE_C5, E},
+
+    {NOTE_F5, E},
+    {NOTE_F5, E},
+    {NOTE_F5, E},
+    {NOTE_F5, E},
+    // --------------
+    {NOTE_G5, E},
+    {NOTE_G5, E},
+    {NOTE_G5, E},
+    {NOTE_G5, E},
+
+    {NOTE_G5, E},
+    {NOTE_G5, E},
+    {NOTE_G5, E},
+    {NOTE_G5, E},
+
+    {NOTE_G5, Q},
+    {NOTE_G5, Q},
+
+    {NOTE_C5, E},
+    {NOTE_Bb4, E},
+    {NOTE_LA4, E},
+    {NOTE_F4, E},
+    // --------------
+    {NOTE_G4, Q},
+    {NOTE_G4, E},
+    {NOTE_D5, E},
+
+    {NOTE_C5, Q},
+    {NOTE_Bb4, Q},
+
+    {NOTE_LA4, Q},
+    {NOTE_LA4, E},
+    {NOTE_LA4, E},
+
+    {NOTE_C5, Q},
+    {NOTE_Bb4, E},
+    {NOTE_LA4, E},
+    // --------------
+    {NOTE_G4, Q},
+    {NOTE_G4, E},
+    {NOTE_Ab5, E},
+
+    {NOTE_LA5, E},
+    {NOTE_Ab5, E},
+    {NOTE_LA5, E},
+    {NOTE_Ab5, E},
+
+    {NOTE_G4, Q},
+    {NOTE_G4, E},
+    {NOTE_Ab5, E},
+
+    {NOTE_LA5, E},
+    {NOTE_Ab5, E},
+    {NOTE_LA5, E},
+    {NOTE_Ab5, E},
+    // --------------
+    {NOTE_G4, Q},
+    {NOTE_G4, E},
+    {NOTE_D5, E},
+
+    {NOTE_C5, Q},
+    {NOTE_Bb4, Q},
+
+    {NOTE_LA4, Q},
+    {NOTE_LA4, E},
+    {NOTE_LA4, E},
+
+    {NOTE_C5, Q},
+    {NOTE_Bb4, E},
+    {NOTE_LA4, E},
+    // --------------
+    {NOTE_G4, Q},
+    {NOTE_G4, E},
+    {NOTE_Ab5, E},
+
+    {NOTE_LA5, E},
+    {NOTE_Ab5, E},
+    {NOTE_LA5, E},
+    {NOTE_Ab5, E},
+
+    {NOTE_G4, Q},
+    {NOTE_G4, E},
+    {NOTE_Ab5, E},
+
+    {NOTE_LA5, E},
+    {NOTE_Ab5, E},
+    {NOTE_LA5, E},
+    {NOTE_Ab5, E},
+    // --------------
+    {NOTE_G4, Q},
+
+};
+
+ledc_timer_config_t ledc_timer;
+ledc_channel_config_t ledc_channel;
+
+void buzzer_init()
+{
+    // Pulse Width Modulation (PWM) initialization
+    // https://circuitdigest.com/tutorial/what-is-pwm-pulse-width-modulation
+    ledc_timer.duty_resolution = LEDC_TIMER_13_BIT;
+    ledc_timer.freq_hz = 3000;
+    ledc_timer.speed_mode = LEDC_HIGH_SPEED_MODE;
+    ledc_timer.timer_num = LEDC_TIMER_0;
+
+    ledc_channel.channel = LEDC_CHANNEL_0;
+    ledc_channel.duty = 0;
+    ledc_channel.gpio_num = PIN_BUZZER;
+    ledc_channel.speed_mode = LEDC_HIGH_SPEED_MODE;
+    ledc_channel.timer_sel = LEDC_TIMER_0;
+
+    // Set configuration of timer0 for high speed channels
+    ledc_timer_config(&ledc_timer);
+
+    ledc_channel_config(&ledc_channel);
+
+    // Initialize fade service.
+    ledc_fade_func_install(0);
+}
+
+void buzzer_play_tone(int note, int duration)
+{
+    // Set the note's frequency
+    ledc_set_freq(ledc_channel.speed_mode, ledc_channel.channel, note);
+    // Set the note's duration
+    ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, duration);
+    // update
+    ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
+
+    vTaskDelay(duration / portTICK_PERIOD_MS);
+
+    // Set back to the default values
+    ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, 0);
+    ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
+}
+
+void buzzer_play_song(void * pvParameters)
+{
+    for (int i = 0; i < sizeof(music0) / sizeof(playback_t); i++) {
+        buzzer_play_tone(music0[i].note, music0[i].duration);
+    }
+    buzzer_play_tone(0, Q);
+    for (int i = 0; i < sizeof(music1) / sizeof(playback_t); i++) {
+        buzzer_play_tone(music1[i].note, music1[i].duration);
+    }
+}
---
+++ esp32/components/buzzer/buzzer.h
@@ -0,0 +1,124 @@
+#pragma once
+
+// http://www.phy.mtu.edu/~suits/notefreqs.html
+// Put NOTE_ as prefix to avoid redefinition
+#define NOTE_C0 16.35
+#define NOTE_Db0 17.32
+#define NOTE_D0 18.35
+#define NOTE_Eb0 19.45
+#define NOTE_E0 20.60
+#define NOTE_F0 21.83
+#define NOTE_Gb0 23.12
+#define NOTE_G0 24.50
+#define NOTE_Ab0 25.96
+#define NOTE_LA0 27.50
+#define NOTE_Bb0 29.14
+#define NOTE_B0 30.87
+#define NOTE_C1 32.70
+#define NOTE_Db1 34.65
+#define NOTE_D1 36.71
+#define NOTE_Eb1 38.89
+#define NOTE_E1 41.20
+#define NOTE_F1 43.65
+#define NOTE_Gb1 46.25
+#define NOTE_G1 49.00
+#define NOTE_Ab1 51.91
+#define NOTE_LA1 55.00
+#define NOTE_Bb1 58.27
+#define NOTE_B1 61.74
+#define NOTE_C2 65.41
+#define NOTE_Db2 69.30
+#define NOTE_D2 73.42
+#define NOTE_Eb2 77.78
+#define NOTE_E2 82.41
+#define NOTE_F2 87.31
+#define NOTE_Gb2 92.50
+#define NOTE_G2 98.00
+#define NOTE_Ab2 103.83
+#define NOTE_LA2 110.00
+#define NOTE_Bb2 116.54
+#define NOTE_B2 123.47
+#define NOTE_C3 130.81
+#define NOTE_Db3 138.59
+#define NOTE_D3 146.83
+#define NOTE_Eb3 155.56
+#define NOTE_E3 164.81
+#define NOTE_F3 174.61
+#define NOTE_Gb3 185.00
+#define NOTE_G3 196.00
+#define NOTE_Ab3 207.65
+#define NOTE_LA3 220.00
+#define NOTE_Bb3 233.08
+#define NOTE_B3 246.94
+#define NOTE_C4 261.63
+#define NOTE_Db4 277.18
+#define NOTE_D4 293.66
+#define NOTE_Eb4 311.13
+#define NOTE_E4 329.63
+#define NOTE_F4 349.23
+#define NOTE_Gb4 369.99
+#define NOTE_G4 392.00
+#define NOTE_Ab4 415.30
+#define NOTE_LA4 440.00
+#define NOTE_Bb4 466.16
+#define NOTE_B4 493.88
+#define NOTE_C5 523.25
+#define NOTE_Db5 554.37
+#define NOTE_D5 587.33
+#define NOTE_Eb5 622.25
+#define NOTE_E5 659.26
+#define NOTE_F5 698.46
+#define NOTE_Gb5 739.99
+#define NOTE_G5 783.99
+#define NOTE_Ab5 830.61
+#define NOTE_LA5 880.00
+#define NOTE_Bb5 932.33
+#define NOTE_B5 987.77
+#define NOTE_C6 1046.50
+#define NOTE_Db6 1108.73
+#define NOTE_D6 1174.66
+#define NOTE_Eb6 1244.51
+#define NOTE_E6 1318.51
+#define NOTE_F6 1396.91
+#define NOTE_Gb6 1479.98
+#define NOTE_G6 1567.98
+#define NOTE_Ab6 1661.22
+#define NOTE_LA6 1760.00
+#define NOTE_Bb6 1864.66
+#define NOTE_B6 1975.53
+#define NOTE_C7 2093.00
+#define NOTE_Db7 2217.46
+#define NOTE_D7 2349.32
+#define NOTE_Eb7 2489.02
+#define NOTE_E7 2637.02
+#define NOTE_F7 2793.83
+#define NOTE_Gb7 2959.96
+#define NOTE_G7 3135.96
+#define NOTE_Ab7 3322.44
+#define NOTE_LA7 3520.01
+#define NOTE_Bb7 3729.31
+#define NOTE_B7 3951.07
+#define NOTE_C8 4186.01
+#define NOTE_Db8 4434.92
+#define NOTE_D8 4698.64
+#define NOTE_Eb8 4978.03
+
+#define BPM 103 // for the imperial march
+
+// From the formula:
+// 60,000 / Bpm = Ms We can break this down as 60'000 (the number of
+// milliseconds in a minute).
+
+#define Q 60000 / BPM // quarter 1/4
+#define H 2 * Q       // half 2/4
+#define E Q / 2       // eighth 1/8
+#define S Q / 4       // sixteenth 1/16
+#define W 4 * Q       // whole 4/4
+
+typedef struct {
+    int note;
+    int duration;
+} playback_t;
+
+void buzzer_init();
+void buzzer_play_song(void * pvParameters);
