af94e96190bec31505d67ddf3f9b45108bfe6a93
20th <1331328+20th@users.noreply.github.com>
2021-03-01 22:20:31 -0500

    Implement Konami code sequence handling
    For now, just abort the execution to show that something happened.
    This needs some real handler in the future.

--- esp32/components/rpg/rpg_action.cpp
+++ esp32/components/rpg/rpg_action.cpp
@@ -18,12 +18,61 @@ static ACTION handle_main_enter_action(Scene *scene, int tile_x, int tile_y)
     return ACTION::nothing;
 }
 
+static void handle_main_konami_code(Scene *scene, button_t button,
+                                    struct timeval &time)
+{
+    static uint32_t konami_code = 0;
+    static uint32_t konami_code_timer = 0;
+
+    unsigned int time_usec =
+        (int64_t)time.tv_sec * 1000000L + (int64_t)time.tv_usec;
+
+    if (time_usec - konami_code_timer > 1000000L)
+        konami_code = 0;
+
+    konami_code_timer = time_usec;
+
+    switch (button) {
+    case BUTTON_BACK:
+        konami_code = (konami_code << 3) + 0b001;
+        break;
+
+    case BUTTON_DOWN:
+        konami_code = (konami_code << 3) + 0b010;
+        break;
+
+    case BUTTON_ENTER:
+        konami_code = (konami_code << 3) + 0b011;
+        break;
+
+    case BUTTON_LEFT:
+        konami_code = (konami_code << 3) + 0b100;
+        break;
+
+    case BUTTON_RIGHT:
+        konami_code = (konami_code << 3) + 0b101;
+        break;
+
+    case BUTTON_UP:
+        konami_code = (konami_code << 3) + 0b110;
+        break;
+
+    default:
+        break;
+    }
+
+    if (konami_code == 910842187)
+        abort();
+}
+
 ACTION rpg_action_main_handle(Scene *scene, button_t button, void *extra_arg)
 {
     ACTION ret = ACTION::nothing;
 
     struct timeval now;
     gettimeofday(&now, NULL);
+
+    handle_main_konami_code(scene, button, now);
 
     if (button == BUTTON_ENTER_RELEASE) {
         MainCharacter *mc = scene->get_main_character();
