b5ab219b9decbca03046538911432c0cdf66fac2
20th <1331328+20th@users.noreply.github.com>
2021-03-28 01:30:25 -0400

    Prevent NPC from accidentally jumping off scene
    NPC movement is not uniform, it depends on the rendering speed.  It is
    possible for a fast moving character to do a large leap forward and jump
    over a small blocked section of the map.  Make all area outside of the
    map area blocked by default to prevent them jumping off.

--- esp32/components/rpg/rpg/data/BlockedDataReader.cpp
+++ esp32/components/rpg/rpg/data/BlockedDataReader.cpp
@@ -5,6 +5,12 @@ namespace rpg::data
 
 bool BlockedDataReader::is_blocked(GlobalCoordinates coordinates)
 {
+    if (coordinates.x() < 0 || coordinates.y() < 0)
+        return true;
+
+    if (coordinates.x() >= scene_size.x() || coordinates.y() > scene_size.y())
+        return true;
+
     unsigned int offset = ((coordinates.y() / 6) * line_words) +
                           (coordinates.x() / (2 * DISPLAY_TILE_WIDTH));
 
