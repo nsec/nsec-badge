210f6d62d755458b2c5f3a91d1ce7dab3ab6e595
20th <1331328+20th@users.noreply.github.com>
2021-04-03 12:09:44 -0400

    Pause the scene and display signs as an overlay in the correct map locations

--- esp32/components/rpg/rpg_action.cpp
+++ esp32/components/rpg/rpg_action.cpp
@@ -9,12 +9,78 @@ namespace rpg
 
 using ACTION = ControlExitAction;
 
-static ACTION handle_main_enter_action(Scene *scene,
-                                       GlobalCoordinates coordinates)
+enum class Oversign {
+    badger,
+    customs,
+    hut,
+    port,
+    quack,
+};
+
+static ACTION handle_show_oversign(Oversign oversign, Scene *scene)
 {
+    scene->pause();
+    scene->lock();
+
+    switch (oversign) {
+    case Oversign::badger:
+        graphics_draw_from_library(LIBRARY_IMAGE_OVERSIGN_BADGER, 0, 0);
+        break;
+
+    case Oversign::customs:
+        graphics_draw_from_library(LIBRARY_IMAGE_OVERSIGN_CUSTOMS, 0, 0);
+        break;
+
+    case Oversign::hut:
+        graphics_draw_from_library(LIBRARY_IMAGE_OVERSIGN_HUT, 0, 0);
+        break;
+
+    case Oversign::port:
+        graphics_draw_from_library(LIBRARY_IMAGE_OVERSIGN_PORT, 0, 0);
+        break;
+
+    case Oversign::quack:
+        graphics_draw_from_library(LIBRARY_IMAGE_OVERSIGN_QUACK, 0, 0);
+        break;
+    }
+
+    graphics_update_display();
+
+    // Block control until the user breaks out by pressing back.
+    button_t button = BUTTON_NONE;
+    do {
+        xQueueReceive(button_event_queue, &button, 10 / portTICK_PERIOD_MS);
+    } while (button != BUTTON_BACK_RELEASE);
+
+    scene->get_viewport().mark_for_full_refresh();
+    scene->unlock();
+    scene->unpause();
+
+    return ACTION::nothing;
+}
+
+static ACTION handle_main_enter_action(Scene *scene)
+{
+    auto coordinates = scene->get_main_character()->get_coordinates();
+
     // Active area near the Door #6.
     if (coordinates.within_tile(31, 42, 32, 43))
         return ACTION::exit;
+
+    if (coordinates.within_xy(100, 360, 140, 390))
+        return handle_show_oversign(Oversign::hut, scene);
+
+    if (coordinates.within_xy(200, 1010, 240, 1035))
+        return handle_show_oversign(Oversign::badger, scene);
+
+    if (coordinates.within_xy(485, 985, 525, 1010))
+        return handle_show_oversign(Oversign::customs, scene);
+
+    if (coordinates.within_xy(485, 530, 525, 550))
+        return handle_show_oversign(Oversign::quack, scene);
+
+    if (coordinates.within_xy(1000, 675, 1040, 700))
+        return handle_show_oversign(Oversign::port, scene);
 
     return ACTION::nothing;
 }
@@ -76,8 +142,7 @@ ACTION rpg_action_main_handle(Scene *scene, button_t button, void *extra_arg)
         return ACTION::konami_code;
 
     if (button == BUTTON_ENTER_RELEASE) {
-        auto coordinates = scene->get_main_character()->get_coordinates();
-        ret = handle_main_enter_action(scene, coordinates);
+        ret = handle_main_enter_action(scene);
     }
 
     return ret;
