3e602d47cc97a10586cc7e0f560c1d9147f210cb
20th <1331328+20th@users.noreply.github.com>
2021-04-03 12:09:44 -0400

    Display a dialog box on an interaction with an NPC

--- esp32/components/rpg/rpg_action.cpp
+++ esp32/components/rpg/rpg_action.cpp
@@ -2,6 +2,7 @@
 
 #include "rpg/Coordinates.h"
 #include "rpg/Viewport.h"
+#include "rpg/characters/CharacterDuck.h"
 #include "rpg/characters/MainCharacter.h"
 
 namespace rpg
@@ -16,6 +17,108 @@ enum class Oversign {
     port,
     quack,
 };
+
+static bool display_character_dialog(Character *character, Scene *scene,
+                                     const char **dialog, int &start_line,
+                                     int change)
+{
+    constexpr int dialog_position_x = 40;
+    constexpr int dialog_position_y = 65;
+
+    // Look for an array terminator and hope for the best. So much for memory
+    // safety.
+    int total_lines = 0;
+    for (; dialog[total_lines][0] != '\0'; ++total_lines)
+        assert(total_lines < 40 &&
+               "Giving up looking for a terminator in a dialog array.");
+
+    int line = start_line + change;
+    if (line > total_lines - 6)
+        line = total_lines - 6;
+
+    if (line < 0)
+        line = 0;
+
+    if (change != 0 && line == start_line)
+        return true;
+
+    start_line = line;
+
+    // Workaround the problem that the printed text bypasses the display
+    // buffer. As the result, the buffer content does not change between two
+    // draws of the dialog panel and nothing is actually sent to display.
+    graphics_draw_from_library(LIBRARY_IMAGE_DIALOG_PANEL, 0, 1);
+    graphics_draw_from_library(LIBRARY_IMAGE_DIALOG_PANEL, 0, 0);
+    graphics_update_display();
+
+    int color = 0x39c4;
+    int x = dialog_position_x;
+    int y = dialog_position_y;
+
+    graphics_print_large(character->get_name(), x, y, color, &x, &y);
+    graphics_print_large("_\n", x, y, color, &x, &y);
+
+    x = dialog_position_x;
+    for (; line < start_line + 6 && dialog[line][0] != '\0'; ++line)
+        graphics_print_small(dialog[line], x, y, color, &x, &y);
+
+    return true;
+}
+
+static ACTION handle_character_interaction(Character *character, Scene *scene)
+{
+    button_t button = BUTTON_NONE;
+    bool displayed = false;
+    int start_line = 0;
+
+    scene->pause();
+    scene->lock();
+
+    const char **dialog = character->get_dialog();
+    if (dialog) {
+        // Make the dialog popup more legible before displaying anything.
+        graphics_fadeout_display_buffer(50);
+
+        // Display the dialog the first time and then go on to do more time
+        // consuming things - the user will have something to read.
+        displayed =
+            display_character_dialog(character, scene, dialog, start_line, 0);
+    }
+
+    if (CharacterDuck::identify(character)) {
+        // FIXME
+    }
+
+    while (displayed) {
+        xQueueReceive(button_event_queue, &button, 10 / portTICK_PERIOD_MS);
+
+        switch (button) {
+        case BUTTON_BACK_RELEASE:
+            // Go on to break from the while loop.
+            displayed = false;
+            break;
+
+        case BUTTON_DOWN:
+            displayed = display_character_dialog(character, scene, dialog,
+                                                 start_line, +5);
+            break;
+
+        case BUTTON_UP:
+            displayed = display_character_dialog(character, scene, dialog,
+                                                 start_line, -5);
+            break;
+
+        default:
+            continue;
+        }
+    }
+
+    scene->get_viewport().mark_for_full_refresh();
+    scene->unlock();
+    scene->unpause();
+
+    return ACTION::nothing;
+}
 
 static ACTION handle_show_oversign(Oversign oversign, Scene *scene)
 {
@@ -61,7 +164,8 @@ static ACTION handle_show_oversign(Oversign oversign, Scene *scene)
 
 static ACTION handle_main_enter_action(Scene *scene)
 {
-    auto coordinates = scene->get_main_character()->get_coordinates();
+    auto mc = scene->get_main_character();
+    auto coordinates = mc->get_coordinates();
 
     // Active area near the Door #6.
     if (coordinates.within_tile(31, 42, 32, 43))
@@ -81,6 +185,19 @@ static ACTION handle_main_enter_action(Scene *scene)
 
     if (coordinates.within_xy(1000, 675, 1040, 700))
         return handle_show_oversign(Oversign::port, scene);
+
+    for (auto character : scene->get_characters()) {
+        if (character == mc)
+            continue;
+
+        auto character_coordinates = character->get_coordinates();
+        int delta_x = character_coordinates.x() - coordinates.x();
+        int delta_y = character_coordinates.y() - coordinates.y();
+
+        int distance_sq = (delta_x * delta_x) + (delta_y * delta_y);
+        if (distance_sq < 600)
+            return handle_character_interaction(character, scene);
+    }
 
     return ACTION::nothing;
 }
