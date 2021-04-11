f071cd79f354e1596ec3968d31b102f6a19b6657
20th <1331328+20th@users.noreply.github.com>
2021-04-11 18:08:11 -0400

    Display sample icons on the status bar

--- esp32/components/rpg/rpg_statusbar.cpp
+++ esp32/components/rpg/rpg_statusbar.cpp
@@ -4,12 +4,27 @@ namespace rpg
 {
 
 constexpr auto statusbar_position = viewport_crop_height;
+constexpr auto statusbar_icons_position = statusbar_position + 4;
 
 void rpg_statusbar_render(Scene *scene)
 {
     scene->lock();
 
     graphics_draw_from_library(LIBRARY_IMAGE_STATUS_BAR, 0, statusbar_position);
+
+    for (int i = 0; i < 10; ++i) {
+        if (i % 3 == 0)
+            graphics_draw_from_library(LIBRARY_IMAGE_STATUS_BAR_FLAG_ON, i * 14,
+                                       statusbar_icons_position);
+        else
+            graphics_draw_from_library(LIBRARY_IMAGE_STATUS_BAR_FLAG_OFF,
+                                       i * 14, statusbar_icons_position);
+    }
+
+    graphics_draw_from_library(LIBRARY_IMAGE_STATUS_BAR_WIFI_OFF, 200,
+                               statusbar_icons_position);
+    graphics_draw_from_library(LIBRARY_IMAGE_STATUS_BAR_SOUND_ON, 216,
+                               statusbar_icons_position);
     graphics_update_display();
 
     scene->unlock();
