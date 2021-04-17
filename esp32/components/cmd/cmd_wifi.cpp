48eaf00f19d4d6214586ab844efa96cf44fc4c39
20th <1331328+20th@users.noreply.github.com>
2021-04-17 18:15:13 -0400

    Set hostname on both Wifi network interfaces

--- esp32/components/cmd/cmd_wifi.cpp
+++ esp32/components/cmd/cmd_wifi.cpp
@@ -53,10 +53,11 @@ static void initialise_wifi(void)
     wifi_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_t *ap_netif = esp_netif_create_default_wifi_ap();
-    esp_netif_set_hostname(ap_netif, HOSTNAME);
     assert(ap_netif);
+    esp_netif_set_hostname(ap_netif, HOSTNAME);
     esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
     assert(sta_netif);
+    esp_netif_set_hostname(sta_netif, HOSTNAME);
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_event_handler_register(
