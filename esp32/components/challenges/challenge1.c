30ad8ddcf40e6591f49fa620f5c5e63038b48c8d
20th <1331328+20th@users.noreply.github.com>
2021-04-24 12:10:56 -0400

    Apply code formatting to the 'challenges' component

--- esp32/components/challenges/challenge1.c
+++ esp32/components/challenges/challenge1.c
@@ -2,63 +2,68 @@
 
 static const int ARRAY_LEN = 32;
 
-static int
-verify(const char *user_code) {
-  int code[ARRAY_LEN];
+static int verify(const char *user_code)
+{
+    int code[ARRAY_LEN];
 
-  // expected user input: 11a922186c46496eb1317c128b361720
-  code[21] = 99;
-  code[16] = 98;
-  code[10] = 52;
-  code[18] = 51;
-  code[24] = 56;
-  code[25] = 98;
-  code[5] = 50;
-  code[0] = 49;
-  code[23] = 50;
-  code[22] = 49;
-  code[29] = 55;
-  code[3] = 57;
-  code[19] = 49;
-  code[2] = 97;
-  code[15] = 101;
-  code[17] = 49;
-  code[1] = 49;
-  code[6] = 49;
-  code[31] = 48;
-  code[14] = 54;
-  code[4] = 50;
-  code[7] = 56;
-  code[27] = 54;
-  code[20] = 55;
-  code[8] = 54;
-  code[12] = 52;
-  code[28] = 49;
-  code[13] = 57;
-  code[30] = 50;
-  code[11] = 54;
-  code[9] = 99;
-  code[26] = 51;
+    // expected user input: 11a922186c46496eb1317c128b361720
+    code[21] = 99;
+    code[16] = 98;
+    code[10] = 52;
+    code[18] = 51;
+    code[24] = 56;
+    code[25] = 98;
+    code[5] = 50;
+    code[0] = 49;
+    code[23] = 50;
+    code[22] = 49;
+    code[29] = 55;
+    code[3] = 57;
+    code[19] = 49;
+    code[2] = 97;
+    code[15] = 101;
+    code[17] = 49;
+    code[1] = 49;
+    code[6] = 49;
+    code[31] = 48;
+    code[14] = 54;
+    code[4] = 50;
+    code[7] = 56;
+    code[27] = 54;
+    code[20] = 55;
+    code[8] = 54;
+    code[12] = 52;
+    code[28] = 49;
+    code[13] = 57;
+    code[30] = 50;
+    code[11] = 54;
+    code[9] = 99;
+    code[26] = 51;
 
-  for (int i = 0; i < ARRAY_LEN; ++i) {
-    if (code[i] != user_code[i]) {
-      return 0;
+    for (int i = 0; i < ARRAY_LEN; ++i) {
+        if (code[i] != user_code[i]) {
+            return 0;
+        }
     }
-  }
-  return 1;
+
+    return 1;
 }
 
-void
-challenge_RE101(const char *user_code) {
-  if (verify(user_code)) {
-    // flag-REverSINg_xteNSA_is_NOt_that_HArd
-    const char flag[] = "\x64\x6e\x63\x65\x2f\x50\x47\x74\x67\x70\x51\x4b\x4c\x65\x5d\x7a\x76\x67\x4c\x51\x43\x5d\x6b\x71\x5d\x4c\x4d\x76\x5d\x76\x6a\x63\x76\x5d\x4a\x43\x70\x66";
-    for (int i = 0; i < 38; ++i) {
-      printf("%c", flag[i] ^ 0x2);
+void challenge_RE101(const char *user_code)
+{
+    if (verify(user_code)) {
+        // flag-REverSINg_xteNSA_is_NOt_that_HArd
+        const char flag[] =
+            "\x64\x6e\x63\x65\x2f\x50\x47\x74\x67\x70\x51\x4b\x4c\x65\x5d\x7a"
+            "\x76\x67\x4c\x51\x43\x5d\x6b\x71\x5d\x4c\x4d\x76\x5d\x76\x6a\x63"
+            "\x76\x5d\x4a\x43\x70\x66";
+
+        for (int i = 0; i < 38; ++i) {
+            printf("%c", flag[i] ^ 0x2);
+        }
+
+        printf("\n");
+    } else {
+        printf("Nope try again\n");
     }
-    printf("\n");
-  }
-  else{
-    printf("Nope try again\n");
-  }
 }
--- esp32/components/challenges/challenges.c
+++ esp32/components/challenges/challenges.c
@@ -1,6 +1,6 @@
+#include "argtable3/argtable3.h"
 #include "challenge1.h"
 #include "challenge2.h"
-#include "argtable3/argtable3.h"
 #include "esp_console.h"
 #include "esp_log.h"
 #include <string.h>
@@ -11,8 +11,8 @@ static struct {
     struct arg_end *end;
 } challenges_args;
 
-
-static int challenges(int argc, char **argv) {
+static int challenges(int argc, char **argv)
+{
     int nerrors = arg_parse(argc, argv, (void **)&challenges_args);
     char challenge1[5] = "RE101";
     char challenge2[5] = "RE102";
@@ -21,39 +21,37 @@ static int challenges(int argc, char **argv) {
         arg_print_errors(stderr, challenges_args.end, argv[0]);
         return 1;
     }
+
     if (!strncmp(challenge1, challenges_args.type->sval[0], 5)) {
         printf("checking RE101\n");
-        ESP_LOGI(
-            __func__,
-            "Submitting code for %s",
-            challenges_args.type->sval[0]);
+        ESP_LOGI(__func__, "Submitting code for %s",
+                 challenges_args.type->sval[0]);
 
         challenge_RE101(challenges_args.code->sval[0]);
-    }
-    else if (!strncmp(challenge2, challenges_args.type->sval[0], 5)) {
+    } else if (!strncmp(challenge2, challenges_args.type->sval[0], 5)) {
         printf("checking RE102\n");
-        ESP_LOGI(
-            __func__,
-            "Submitting code for %s",
-            challenges_args.type->sval[0]);
+        ESP_LOGI(__func__, "Submitting code for %s",
+                 challenges_args.type->sval[0]);
+
         challenge_RE102(challenges_args.code->sval[0]);
-    }
-    else {
+    } else {
         ESP_LOGW(__func__, "Type unknown");
     }
+
     return 0;
 }
 
-void register_challenges(void) {
+void register_challenges(void)
+{
     challenges_args.type = arg_str0(NULL, NULL, "<RE(101|102)>", NULL);
     challenges_args.code = arg_str1(NULL, NULL, "<code>", NULL);
     challenges_args.end = arg_end(2);
 
-    const esp_console_cmd_t challenges_cmd = {
-        .command = "challenges",
-        .help = "???",
-        .hint = NULL,
-        .func = &challenges,
-        .argtable = &challenges_args};
+    const esp_console_cmd_t challenges_cmd = {.command = "challenges",
+                                              .help = "???",
+                                              .hint = NULL,
+                                              .func = &challenges,
+                                              .argtable = &challenges_args};
+
     ESP_ERROR_CHECK(esp_console_cmd_register(&challenges_cmd));
 }
