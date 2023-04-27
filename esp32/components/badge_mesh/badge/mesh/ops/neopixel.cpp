1fa8cb840ed5e8e887fa313014096f20faaaf525
EiNSTeiN- <einstein@g3nius.org>
2023-04-27 16:09:19 -0400

    Improve neopixel mesh operation

--- esp32/components/badge_mesh/badge/mesh/ops/neopixel.cpp
+++ esp32/components/badge_mesh/badge/mesh/ops/neopixel.cpp
@@ -33,12 +33,23 @@ esp_err_t neopixel_set_received(esp_ble_mesh_model_t *model, esp_ble_mesh_msg_ct
     bool time_elapsed = neopixel_set.recv_at == 0 || elapsed_time_ms > (neopixel_set.data.time * 1000);
     bool stronger_signal = ctx->recv_rssi > neopixel_set.recv_rssi;
     bool high_priority = data->flags & NEOPIXEL_FLAG_HIGH_PRIORITY;
-    bool should_obey = time_elapsed || stronger_signal || (high_priority && !neopixel_set.high_priority);
+    bool same_sender = neopixel_set.recv_from == ctx->addr;
+    bool should_obey = same_sender || time_elapsed || (stronger_signal && !neopixel_set.high_priority) || (high_priority && !neopixel_set.high_priority);
+
+    ESP_LOGV(TAG, "%s: time_elapsed=%s stronger_signal=%s high_priority=%s (old %s) same_sender=%s should_obey=%s", __func__,
+        time_elapsed ? "yes" : "no",
+        stronger_signal ? "yes" : "no",
+        high_priority ? "yes" : "no",
+        neopixel_set.high_priority ? "yes" : "no",
+        same_sender ? "yes" : "no",
+        should_obey ? "yes" : "no"
+    );
 
     /*
         As a way to tame the chaos, only switch mode if:
+            - the previous command was from the same sender, or
             - the minimum time has elapsed, or
-            - the new signal is stronger, or
+            - the new signal is stronger and the current one isn't high priority, or
             - the new command is high priority and the previous command wasn't.
     */
     if(!should_obey) {
@@ -56,7 +67,7 @@ esp_err_t neopixel_set_received(esp_ble_mesh_model_t *model, esp_ble_mesh_msg_ct
 
     NeoPixel::getInstance().setColor((int)data->color);
     NeoPixel::getInstance().setBrightness(data->brightness);
-    NeoPixel::getInstance().setMode(data->mode);
+    NeoPixel::getInstance().setPublicMode(data->mode);
 
     return ESP_OK;
 }
