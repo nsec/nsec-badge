8e17e2a97d7b01f15c1574f3345f8d60f281f20c
jmarcil <jonathan.marcil@owasp.org>
2024-03-22 15:06:42 -0400

    cleaning up comments

--- esp32/components/off_the_addon/ota_init.h
+++ esp32/components/off_the_addon/ota_init.h
@@ -2,36 +2,37 @@
 
 #include "driver/gpio.h"
 
-#define SAO1_IO1 GPIO_NUM_3 // white
-#define SAO1_IO2 GPIO_NUM_4 // yellow
-
-#define SAO2_IO1 GPIO_NUM_5 // blue
-#define SAO2_IO2 GPIO_NUM_6 // green
-
+// Off the addon PINs (used in all firmware)
 #define SAO3_I01 GPIO_NUM_7
 #define ADDON_BLUE_LED SAO3_I01 // blue LED on addon
-
 #define SAO3_I02 GPIO_NUM_8
 #define ADDON_DETECT SAO3_I02 // resistance to ground on addon
 
+// CTF ADDON PINs (used in CTF ADDON firmware, defined in all to reserve them)
+#define SAO1_IO1 GPIO_NUM_3
+#define SAO1_IO2 GPIO_NUM_4
+#define SAO2_IO1 GPIO_NUM_5
+#define SAO2_IO2 GPIO_NUM_6
+
+// Not used by the CTF ADDON
 #define SAO4_IO1 GPIO_NUM_9
 #define SAO4_IO2 GPIO_NUM_10
 
+// SPI configuration for the external flash on CTF ADDON
 #define HOST_ID  SPI3_HOST
-#define PIN_CS   SAO1_IO1 // white
-#define PIN_CLK  SAO1_IO2 // yellow
-#define PIN_MOSI SAO2_IO1 // blue
-#define PIN_MISO SAO2_IO2 // green
+#define PIN_CS   SAO1_IO1
+#define PIN_CLK  SAO1_IO2
+#define PIN_MOSI SAO2_IO1
+#define PIN_MISO SAO2_IO2
 /* Those two PINs are only used for SPI quad mode, which we don't use
 #define PIN_WP // connected to ground on the addon
 #define PIN_HD // user will connect to any VCC
 */
 #define SPI_DMA_CHAN SPI_DMA_CH_AUTO
 
+// OTA configuration
 #define NSEC_CONF_PARTITION ESP_PARTITION_SUBTYPE_APP_FACTORY
-
 #define NSEC_CTF_PARTITION ESP_PARTITION_SUBTYPE_APP_OTA_0
-
 #define NSEC_OTA_PARTITION ESP_PARTITION_SUBTYPE_APP_OTA_1
 #define NSEC_OTA_ID (NSEC_OTA_PARTITION - ESP_PARTITION_SUBTYPE_APP_OTA_MIN)
 
