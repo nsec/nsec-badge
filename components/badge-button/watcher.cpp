50ecca10914ad8e4b97471601e8163139a78176c
Sylvain Lemieux <sylemieux.git@gmail.com>
2024-03-26 17:08:33 -0400

    watcher: Initial clean-up of the module
    Initial clean-up of the watcher module.
    Removed the denouncer capability (handle by IOT button).
    
    Signed-off-by: Sylvain Lemieux <sylemieux.git@gmail.com>

--- components/badge-button/watcher.cpp
+++ components/badge-button/watcher.cpp
@@ -43,94 +43,9 @@ void nb::watcher::tick(
 
     // Check the state of all button pins and debounce as needed
     for (auto btn_idx = 0U; btn_idx < btn_count; btn_idx++) {
-        auto &debouncer = _button_debouncers[btn_idx];
-        const auto pin = btn_pins[btn_idx];
-
-        const auto new_event = debouncer.update(digitalRead(pin) == 0);
-        if (new_event == debouncer::event::NONE) {
-            continue;
-        }
+        const auto new_event = digitalRead((int)btn_idx);
 
         _notify_new_event(static_cast<id>(btn_idx),
                           static_cast<event>(new_event));
     }
-}
-
-nb::watcher::debouncer::debouncer()
-{
-    transition_to_state(state::NONE);
-}
-
-void nb::watcher::debouncer::transition_to_state(
-    nb::watcher::debouncer::state new_state)
-{
-    _state = new_state;
-    _ticks_in_state = 0;
-}
-
-nb::watcher::debouncer::event
-nb::watcher::debouncer::update(bool button_state) noexcept
-{
-    const auto state_on_entry = _state;
-
-    _ticks_in_state++;
-
-    switch (state_on_entry) {
-    case state::NONE:
-        if (button_state) {
-            transition_to_state(state::DOWN_CANDIDATE);
-        }
-
-        break;
-    case state::UP_CANDIDATE:
-    case state::DOWN_CANDIDATE:
-        // Up and down candidate states have values 0 and 1 respectively.
-        if (button_state != static_cast<bool>(state_on_entry)) {
-            // bouncing detected, reset.
-            transition_to_state(state::NONE);
-        } else {
-            if (_ticks_in_state == nsec::config::button::debounce_ticks - 1) {
-                // transition confirmed, trigger and move to next state.
-                transition_to_state(state_on_entry == state::UP_CANDIDATE
-                                        ? state::NONE
-                                        : state::DOWN_WAIT_FIRST_REPEAT);
-                return state_on_entry == state::UP_CANDIDATE
-                           ? nb::watcher::debouncer::event::UP
-                           : nb::watcher::debouncer::event::DOWN;
-            }
-        }
-
-        break;
-    case state::DOWN_WAIT_FIRST_REPEAT:
-    case state::DOWN_REPEAT: {
-        constexpr auto ticks_before_first_repeat =
-            nsec::config::button::button_down_first_repeat_delay_ms /
-            nsec::config::button::polling_period_ms;
-        constexpr auto ticks_before_repeat =
-            nsec::config::button::button_down_repeat_delay_ms /
-            nsec::config::button::polling_period_ms;
-
-        const auto ticks_to_wait =
-            state_on_entry == state::DOWN_WAIT_FIRST_REPEAT
-                ? ticks_before_first_repeat
-                : ticks_before_repeat;
-
-        if (!button_state) {
-            transition_to_state(state::UP_CANDIDATE);
-            break;
-        }
-
-        if (_ticks_in_state == ticks_to_wait) {
-            transition_to_state(state::DOWN_REPEAT);
-            return nb::watcher::debouncer::event::DOWN_REPEAT;
-        }
-
-        break;
-    }
-    default:
-        // Unreachable or the cosmic rays are hitting pretty hard...
-        break;
-    }
-
-    return nb::watcher::debouncer::event::NONE;
 }
--- components/badge-button/watcher.hpp
+++ components/badge-button/watcher.hpp
@@ -44,42 +44,6 @@ class watcher : public nsec::scheduling::periodic_task<watcher>
     void tick(scheduling::absolute_time_ms current_time_ms) noexcept;
 
   private:
-    class debouncer
-    {
-      public:
-        debouncer();
-
-        // Deactivate copy and assignment.
-        debouncer(const debouncer &) = delete;
-        debouncer(debouncer &&) = delete;
-        debouncer &operator=(const debouncer &) = delete;
-        debouncer &operator=(debouncer &&) = delete;
-        ~debouncer() = default;
-
-        enum class event {
-            NONE = -1,
-            UP = static_cast<int>(button::event::UP),
-            DOWN = static_cast<int>(button::event::DOWN),
-            DOWN_REPEAT = static_cast<int>(button::event::DOWN_REPEAT),
-        };
-
-        event update(bool button_state) noexcept;
-
-      private:
-        enum class state : uint8_t {
-            UP_CANDIDATE = 0,
-            DOWN_CANDIDATE = 1,
-            DOWN_WAIT_FIRST_REPEAT = 2,
-            DOWN_REPEAT = 3,
-            NONE = 4,
-        };
-
-        void transition_to_state(state new_state);
-
-        state _state;
-        // Ticks since transition into state.
-        uint8_t _ticks_in_state;
-    } _button_debouncers[static_cast<size_t>(id::CANCEL) + 1];
     new_button_event_notifier _notify_new_event;
 };
 
