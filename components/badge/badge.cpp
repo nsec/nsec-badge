nsec/nsec-badge

commit 2bd75aab1069c2d6eff74bb00893ddcc69be9fb1

badge: missing locking on public entry points
Some badge entry points are used without acquiring the public access
semaphore. While it doesn't seem problematic for the moment, that could
lead to hard to trace bugs down the line.

Signed-off-by: Jérémie Galarneau <jeremie.galarneau@gmail.com>

components/badge/badge.cpp
@@ -252,6 +252,7 @@ void nr::badge::on_button_event(nsec::button::id button,
                                 nsec::button::event event) noexcept
 {
     _logger.info("Button event: button={}, event={}", button, event);
+    nsync::lock_guard lock(_public_access_semaphore);
 
     if (_current_network_app_state != network_app_state::UNCONNECTED &&
         _current_network_app_state != network_app_state::IDLE) {
@@ -304,7 +305,6 @@ nc::network_handler::application_message_action
 nr::badge::on_message_received(communication::message::type message_type,
                                const uint8_t *message) noexcept
 {
-    nsync::lock_guard lock(_public_access_semaphore);
     _logger.debug("Received message: type={}", message_type);
 
     if (_current_network_app_state == network_app_state::EXCHANGING_IDS) {
@@ -357,8 +357,9 @@ void nr::badge::_set_network_app_state(
 nr::badge::badge_discovered_result
 nr::badge::on_badge_discovered(const uint8_t *id) noexcept
 {
-    nsec::runtime::badge_unique_id new_id;
+    nsync::lock_guard lock(_public_access_semaphore);
 
+    nsec::runtime::badge_unique_id new_id;
     std::memcpy(new_id.data(), id, new_id.size());
 
     _logger.info("Badge discovered event: badge_id={}", new_id);
@@ -377,6 +378,8 @@ nr::badge::on_badge_discovered(const uint8_t *id) noexcept
 
 void nr::badge::on_badge_discovery_completed() noexcept
 {
+    nsync::lock_guard lock(_public_access_semaphore);
+
     _logger.info("Badge discovery completed");
     _badges_discovered_last_exchange = _id_exchanger.new_badges_discovered();
     _set_network_app_state(network_app_state::ANIMATE_PAIRING_COMPLETED);
@@ -835,6 +838,7 @@ nr::badge::pairing_completed_animator::_animation_state() const noexcept
 
 void nr::badge::clear_leds()
 {
+    nsync::lock_guard lock(_public_access_semaphore);
     _strip_animator.set_blank_animation();
 }
 
@@ -885,7 +889,6 @@ void nr::badge::_set_selected_animation(uint8_t animation_id,
 void nr::badge::_cycle_selected_animation(
     nr::badge::cycle_animation_direction direction) noexcept
 {
-    nsync::lock_guard lock(_public_access_semaphore);
     const auto original_animation_id = _selected_animation;
 
     const auto new_selected_animation = std::clamp<int>(

commit a5b17033ac6d9ca06532b44fd115170caf3d5cfe

badge: restore post synchronization animation
Commit 4bb4dbc3d841 ensure the social level is store as early as possible.

When the synchronization is completed, the animations that highlight
if new badges are discovered were not played.

Fixed the issue by not selecting the idle animation while updating
the social level scoring (i.e. discovered badge).

Fixes: 2bd75aab1069
Signed-off-by: Sylvain Lemieux <sylemieux.git@gmail.com>

components/badge/badge.cpp
@@ -200,7 +200,7 @@ void nr::badge::load_config()
     }
 
     set_social_level(social_level, false);
-    _set_selected_animation(selected_animation_id, false);
+    _set_selected_animation(selected_animation_id, false, true);
 }
 
 void nr::badge::save_config() const
@@ -867,7 +867,7 @@ void nr::badge::apply_score_change(uint16_t new_badges_discovered_count) noexcep
 
     // Saves to configuration
     set_social_level(new_social_level, true);
-    _set_selected_animation(_social_level, true);
+    _set_selected_animation(_social_level, true, false);
 }
 
 uint8_t nr::badge::_compute_new_social_level(
@@ -889,10 +889,13 @@ uint8_t nr::badge::_compute_new_social_level(
 }
 
 void nr::badge::_set_selected_animation(uint8_t animation_id,
-                                        bool save_to_config) noexcept
+                                        bool save_to_config,
+                                        bool set_idle_animation) noexcept
 {
     _selected_animation = animation_id;
-    _strip_animator.set_idle_animation(animation_id);
+    if (set_idle_animation) {
+        _strip_animator.set_idle_animation(animation_id);
+    }
 
     if (save_to_config) {
         save_config();
@@ -912,7 +915,7 @@ void nr::badge::_cycle_selected_animation(
                  "original_animation_id={}, new_animation_id={}",
                  direction, original_animation_id, new_selected_animation);
 
-    _set_selected_animation(new_selected_animation, true);
+    _set_selected_animation(new_selected_animation, true, true);
 
     // Set the LEDs pattern.
     nsec::g::the_badge->_strip_animator.set_idle_animation(

components/badge/badge.hpp
@@ -247,7 +247,8 @@ class badge
     static uint8_t
     _compute_new_social_level(uint8_t current_social_level,
                               uint16_t new_badges_discovered_count) noexcept;
-    void _set_selected_animation(uint8_t animation_id, bool save) noexcept;
+    void _set_selected_animation(uint8_t animation_id, bool save,
+                                 bool set_idle_animation) noexcept;
     // Setup hardware.
     void _setup();
 