nsec/nsec-badge

commit 2bd75aab1069c2d6eff74bb00893ddcc69be9fb1

badge: missing locking on public entry points
Some badge entry points are used without acquiring the public access
semaphore. While it doesn't seem problematic for the moment, that could
lead to hard to trace bugs down the line.

Signed-off-by: Jérémie Galarneau <jeremie.galarneau@gmail.com>

components/badge/badge.cpp
@@ -252,6 +252,7 @@ void nr::badge::on_button_event(nsec::button::id button,
                                 nsec::button::event event) noexcept
 {
     _logger.info("Button event: button={}, event={}", button, event);
+    nsync::lock_guard lock(_public_access_semaphore);
 
     if (_current_network_app_state != network_app_state::UNCONNECTED &&
         _current_network_app_state != network_app_state::IDLE) {
@@ -304,7 +305,6 @@ nc::network_handler::application_message_action
 nr::badge::on_message_received(communication::message::type message_type,
                                const uint8_t *message) noexcept
 {
-    nsync::lock_guard lock(_public_access_semaphore);
     _logger.debug("Received message: type={}", message_type);
 
     if (_current_network_app_state == network_app_state::EXCHANGING_IDS) {
@@ -357,8 +357,9 @@ void nr::badge::_set_network_app_state(
 nr::badge::badge_discovered_result
 nr::badge::on_badge_discovered(const uint8_t *id) noexcept
 {
-    nsec::runtime::badge_unique_id new_id;
+    nsync::lock_guard lock(_public_access_semaphore);
 
+    nsec::runtime::badge_unique_id new_id;
     std::memcpy(new_id.data(), id, new_id.size());
 
     _logger.info("Badge discovered event: badge_id={}", new_id);
@@ -377,6 +378,8 @@ nr::badge::on_badge_discovered(const uint8_t *id) noexcept
 
 void nr::badge::on_badge_discovery_completed() noexcept
 {
+    nsync::lock_guard lock(_public_access_semaphore);
+
     _logger.info("Badge discovery completed");
     _badges_discovered_last_exchange = _id_exchanger.new_badges_discovered();
     _set_network_app_state(network_app_state::ANIMATE_PAIRING_COMPLETED);
@@ -835,6 +838,7 @@ nr::badge::pairing_completed_animator::_animation_state() const noexcept
 
 void nr::badge::clear_leds()
 {
+    nsync::lock_guard lock(_public_access_semaphore);
     _strip_animator.set_blank_animation();
 }
 
@@ -885,7 +889,6 @@ void nr::badge::_set_selected_animation(uint8_t animation_id,
 void nr::badge::_cycle_selected_animation(
     nr::badge::cycle_animation_direction direction) noexcept
 {
-    nsync::lock_guard lock(_public_access_semaphore);
     const auto original_animation_id = _selected_animation;
 
     const auto new_selected_animation = std::clamp<int>(

commit dcec18895b5fa93c756a807958116d997f14df1f

badge: fixed invalid social level display after synchro
Commit 4bb4dbc3d841 ensure the social level is store as early as possible.

When the social level is display, a second computation of the discovered badge is done.
If the previous level was 0, and 2 badges were discovered, 8 will be display instead of 4.

Use the actual level to display the social level after the synchronization is comnpleted.

Fixes: 2bd75aab1069
Signed-off-by: Sylvain Lemieux <sylemieux.git@gmail.com>

components/badge/badge.cpp
@@ -821,16 +821,13 @@ void nr::badge::pairing_completed_animator::_animation_state(
         break;
     case nr::badge::pairing_completed_animator::animation_state::
         SHOW_NEW_LEVEL: {
-        const auto new_level = _compute_new_social_level(
-            badge._social_level, badge._badges_discovered_last_exchange);
-
         badge._strip_animator.set_show_level_animation(
             badge._badges_discovered_last_exchange > 0
                 ? nl::strip_animator::pairing_completed_animation_type::
                       HAPPY_CLOWN_BARF
                 : nl::strip_animator::pairing_completed_animation_type::
                       NO_NEW_FRIENDS,
-            new_level, false);
+            badge._social_level, false);
         break;
     }
     case nr::badge::pairing_completed_animator::animation_state::SHOW_HEALTH: {