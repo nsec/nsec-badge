da77bc8e6d395a1be5dd0d048d4810224df3b452
Hugo Genesse <svieg.ashcasce@gmail.com>
2025-01-19 13:12:33 -0500

    Remove reaction_time

--- components/reaction_time/badge_png/generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Resize yields larger files, even after optimization.
-echo "/* Generated automatically. Do not modify. */"
-# gzip --stdout --best "$1" | base64 --wrap 0 | xxd -i -C -n badge_png
-base64 --wrap 0 "$1" | xxd -i -C -n badge_png
